03e315cae9cdaefa7d86e1f87f32a1bdf770d846
Bartek Malanowski <b.malanowski@mdbootstrap.com>
2018-01-03 10:54:12 +0100

    fixed card image

--- src/components/CardImage.js
+++ src/components/CardImage.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Waves from './Waves';
-import { Mask, View } from 'mdbreact';
+import Mask from './Mask';
+import View from './View';
 
 
 class CardImage extends Component {
