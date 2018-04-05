8bf102a1b6465f984dc6b6ad1fa5654ee7d86de0
Jakub Strebeyko <j.strebeyko@mdbootstrap.com>
2018-04-05 14:47:12 +0200

    Create a StickyPage

--- docs/pages/pro/StickyPage.js
+++ docs/pages/pro/StickyPage.js
@@ -1,49 +1,66 @@
 import React from 'react';
 import ReactDOM, { findDOMNode } from 'react-dom';
+import { Sticky, StickyContainer, Container, Card, CardTitle, CardBody, CardText } from 'mdbreact';
 
+let i = 0;
 
 class StickyPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "Choose your option",
-    }
-    this.optionClick = this.optionClick.bind(this);
-  }
-
-  optionClick(value) {
-    if (value.constructor === Array) {
-      value = value.join(', ');
-    }
-    this.setState({value: value});
-
-  }
-  onClick(e) {
-    // check if select is multiple
-    if (e.target.dataset.multiple === 'true') {
-      return;
-    }
-
-    if (e.target.classList.contains('select-dropdown')) {
-      this.otherDropdownsClose();
-      e.target.nextElementSibling.classList.add('fadeIn');
-    } else {
-      this.otherDropdownsClose();
-    }
-  }
-
-
-
-  componentDidMount() {
-  }
-
-  componentWillUnmount() {
-  }
-
   render() {
     return(
-      <div className="container mt-5 primary-text">
+      <div style={{ margin: 30 }}>
+        <StickyContainer
+          style={{
+            height: '80vh',
+            overflowY: "auto",
+            background: "#ddd",
+            padding: "0 30px"
+          }}
+        >
+          <div style={{ height: 1600 }}>
+            <div style={{ height: 50 }} />
+            <Sticky relative>
+              {({
+                isSticky,
+                wasSticky,
+                style,
+                distanceFromTop,
+                distanceFromBottom,
+                calculatedHeight
+              }) => {
+                // console.log({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight });
 
+                return (
+                  <Card
+                    style={{
+                      ...style,
+                      overflow: "auto",
+                      width: '400px',
+                      marginRight: '12px',
+                      // marginReft: '45vh'
+                    }}
+                  >
+                    <h3 className="card-header primary-color white-text text-center">this is something new</h3>
+                    <CardBody>
+                      <CardTitle>I am a Sticky</CardTitle>
+                      <CardText>Let me travel with you!</CardText>
+                    </CardBody>
+                    <div className="card-footer text-muted primary-color white-text text-right">
+                        <p className="mb-0">{i++} pixels traveled</p>
+                    </div>
+                  </Card>
+                );
+              }}
+            </Sticky>
+
+            <h2 className="text-center" style={{ marginTop: 150 }}>
+              overflowing container
+            </h2>
+            <Container style={{height: 500}}></Container>
+            <h2 className="text-center" style={{ marginTop: 150 }}>
+              hi there!
+            </h2>
+          </div>
+        </StickyContainer>
       </div>
     );
   }
