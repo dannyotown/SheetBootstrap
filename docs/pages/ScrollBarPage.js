7bb7869c502db975e39bd15bfce522548fdd1b2b
Jakub Strebeyko <j.strebeyko@mdbootstrap.com>
2018-04-09 09:30:26 +0200

    PerfectScrollbar

---
+++ docs/pages/ScrollBarPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM, { findDOMNode } from 'react-dom';
+import { Container, Row, Col, PerfectScrollbar } from 'mdbreact';
+// import 'react-perfect-scrollbar/dist/css/styles.css';
+
+
+class ScrollBarPage extends React.Component  {
+
+  render() {
+    const outerContainerStyle = {width: '600px', height: '400px'}
+    const style = { backgroundImage: "url('https://i.imgur.com/nAUUNzH.jpg')",
+    width: "1280px", height: "720px", position: 'relative'}
+    return(
+      <Container style={outerContainerStyle} className="mt-5">
+        <PerfectScrollbar option={{wheelSpeed:1}} className="scrollbar-primary">
+          <Container style={style} className="scrollbar-primary"></Container>
+        </PerfectScrollbar>
+      </Container>
+    );s
+  }
+};
+
+export default ScrollBarPage;
