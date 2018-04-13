799e057ea22c2952a95e074cbfaa896dc7d9e297
Jakub Strebeyko <j.strebeyko@mdbootstrap.com>
2018-04-13 11:04:47 +0200

    fix and simplyfy

--- docs/pages/ScrollBarPage.js
+++ docs/pages/ScrollBarPage.js
@@ -5,13 +5,11 @@ import { Container, PerfectScrollbar } from 'mdbreact';
 class ScrollBarPage extends React.Component  {
 
   render() {
-    const outerContainerStyle = {width: '600px', height: '400px'}
-    const style = { backgroundImage: "url('https://i.imgur.com/nAUUNzH.jpg')",
-    width: "1280px", height: "720px", position: 'relative'}
+    const outerContainerStyle = {width: '800px', height: '400px'}
     return(
       <Container style={outerContainerStyle} className="mt-5">
-        <PerfectScrollbar option={{wheelSpeed:1}} className="scrollbar-primary">
-          <Container style={style} className="scrollbar-primary"></Container>
+        <PerfectScrollbar className="scrollbar-primary">
+          <img src="https://mdbootstrap.com/img/Photos/Others/img%20(51).jpg"/>
         </PerfectScrollbar>
       </Container>
     );
