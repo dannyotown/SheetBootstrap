5acb8ae2ceca81351bc59854e0068b30179b43e2
Jakub Strebeyko <strebeyko@gmx.com>
2018-05-09 14:27:57 +0200

    add FormInline import

--- docs/pages/pro/sections/ClassicFormPage.js
+++ docs/pages/pro/sections/ClassicFormPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, Mask, Row, Col, Fa, Button, View, Container, Card, CardBody, Input } from 'mdbreact';
+import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, NavbarToggler, Collapse, Mask, Row, Col, Fa, Button, View, Container, Card, CardBody, Input, FormInline } from 'mdbreact';
 import './ClassicFormPage.css'
 
 
