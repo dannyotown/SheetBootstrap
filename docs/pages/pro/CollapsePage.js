579667fc2a8fb00d524629be51766f7d6d144b64
Jakub Strebeyko <j.strebeyko@mdbootstrap.com>
2018-08-01 10:47:47 +0200

    Simplify Accordion using currying and arrow functions

--- docs/pages/pro/CollapsePage.js
+++ docs/pages/pro/CollapsePage.js
@@ -4,62 +4,34 @@ import { Container, Collapse, Card, CardBody, CollapseHeader } from 'mdbreact';
 class CollapsePage extends Component {
   constructor(props) {
     super(props);
-    this.onClick1 = this.onClick1.bind(this);
-    this.onClick2 = this.onClick2.bind(this);
-    this.onClick3 = this.onClick3.bind(this);
-
     this.state = {
       accordion: false
     };
   }
-  onClick1() {
-    let state = '';
 
-    if(this.state.accordion !== 1) {
-      state = 1;
-    } else {
-      state = false;
+  onClick = (number) => {
+    return () => {
+      let state = '';
+      if(this.state.accordion !== number) {
+        state = number;
+      } else {
+        state = false;
+      }
+      this.setState({
+        accordion: state});
     }
-
-    this.setState({
-      accordion: state});
-  }
-
-  onClick2() {
-    let state = '';
-
-    if(this.state.accordion !== 2) {
-      state = 2;
-    } else {
-      state = false;
-    }
-
-    this.setState({
-      accordion: state});
-  }
-
-  onClick3() {
-    let state = '';
-
-    if(this.state.accordion !== 3) {
-      state = 3;
-    } else {
-      state = false;
-    }
-
-    this.setState({
-      accordion: state});
   }
 
   render() {
+    const {accordion} = this.state;
     return (
       <Container className="accordion mt-5">
         <h1>Accordion PRO</h1>
         <Card className="mt-3">
-          <CollapseHeader onClick={this.onClick1}>Collapsible Group Item #1
-            <i className={this.state.accordion === 1 ? 'fa fa-angle-down rotate-icon' : 'fa fa-angle-down'}></i>
+          <CollapseHeader onClick={this.onClick(1)}>Collapsible Group Item #1
+            <i className={accordion === 1 ? 'fa fa-angle-down rotate-icon' : 'fa fa-angle-down'}></i>
           </CollapseHeader>
-          <Collapse isOpen={this.state.accordion === 1}>
+          <Collapse isOpen={accordion === 1}>
             <CardBody>
               Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
               non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
@@ -72,10 +44,10 @@ class CollapsePage extends Component {
         </Card>
 
         <Card>
-          <CollapseHeader onClick={this.onClick2}>Collapsible Group Item #2
-            <i className={this.state.accordion === 2 ? 'fa fa-angle-down rotate-icon' : 'fa fa-angle-down'}></i>
+          <CollapseHeader onClick={this.onClick(2)}>Collapsible Group Item #2
+            <i className={accordion === 2 ? 'fa fa-angle-down rotate-icon' : 'fa fa-angle-down'}></i>
           </CollapseHeader>
-            <Collapse isOpen={this.state.accordion === 2}>
+            <Collapse isOpen={accordion === 2}>
               <CardBody>
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
                 non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
@@ -88,10 +60,10 @@ class CollapsePage extends Component {
         </Card>
 
         <Card>
-          <CollapseHeader onClick={this.onClick3}>Collapsible Group Item #3
-            <i className={this.state.accordion === 3 ? 'fa fa-angle-down rotate-icon' : 'fa fa-angle-down'}></i>
+          <CollapseHeader onClick={this.onClick(3)}>Collapsible Group Item #3
+            <i className={accordion === 3 ? 'fa fa-angle-down rotate-icon' : 'fa fa-angle-down'}></i>
           </CollapseHeader>
-            <Collapse isOpen={this.state.accordion === 3}>
+            <Collapse isOpen={accordion === 3}>
               <CardBody>
                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
                 non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
