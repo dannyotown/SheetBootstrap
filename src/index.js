6d83113439bcf1bef512334e70512d81782d8f9a
Jakub Strebeyko <j.strebeyko@mdbootstrap.com>
2018-08-08 13:34:40 +0200

    add react-toastify assets to import/export

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ import TableFoot from './components/TableFoot';
 import TextField from './components/TextField';
 import Tooltip from './components/Tooltip';
 import View from './components/View';
-
+import { toast, ToastContainer} from 'react-toastify';
 // PRO
 import Autocomplete from './components/pro/Autocomplete';
 import Avatar from './components/pro/Avatar';
@@ -297,6 +297,8 @@ export {
   TextField,
   Tooltip,
   View,
+  toast,
+  ToastContainer,
   // PRO
   Autocomplete,
   Avatar,
