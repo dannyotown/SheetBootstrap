e5d5b041a491fa7053977ca1ce53268098020b70
anna-morawska <a.morawska@mdbootstrap.com>
2018-10-16 16:27:28 +0200

    Tweak eslint rules

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
 
 const config = {
   mode: "development",
