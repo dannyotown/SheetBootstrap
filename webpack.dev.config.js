341ff37bec7c63061822fd42bcadac9f30b05697
anna-morawska <a.morawska@mdbootstrap.com>
2018-10-17 11:23:06 +0200

    Remove webpack-bundle-analyzer and babel-runtime

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,8 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
-
 
 const config = {
   mode: "development",
