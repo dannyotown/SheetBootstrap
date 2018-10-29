03541ac22939db390a70f6ef1004717aae0b3204
Jakub Mandra <j.mandra@mdbootstrap.com>
2018-10-29 13:32:13 +0100

    Add additional button and PropTypes to TimePicker footer

--- src/components/pro/TimePicker/ClockpickerFooter.js
+++ src/components/pro/TimePicker/ClockpickerFooter.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ClockpickerFooter = ({ handleDoneClick }) => {
+const propTypes = {
+  clearable: PropTypes.bool.isRequired,
+  handleClearClick: PropTypes.func.isRequired,
+  handleDoneClick: PropTypes.func.isRequired
+};
+
+const ClockpickerFooter = ({ clearable, handleClearClick, handleDoneClick }) => {
   return (
-    <div className="picker__footer">
+    <div className="picker__footer d-flex justify-content-between">
+      {
+        clearable &&
+        <button type="button" className="btn btn-flat clockpicker-button" tabIndex="0" onClick={handleClearClick}>Clear</button>
+      }
       <button type="button" className="btn btn-flat clockpicker-button" tabIndex="0" onClick={handleDoneClick}>Done</button>
     </div>
   );
 }
+
+ClockpickerFooter.propTypes = propTypes;
 
 export default ClockpickerFooter;
