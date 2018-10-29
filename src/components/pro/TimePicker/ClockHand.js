2759cf36d294c0a22efdc78cb5457698d32f5e8e
Jakub Mandra <j.mandra@mdbootstrap.com>
2018-10-29 14:33:23 +0100

    Add PropTypes for ClockHand

--- src/components/pro/TimePicker/ClockHand.js
+++ src/components/pro/TimePicker/ClockHand.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  angle: PropTypes.number.isRequired,
+  scale: PropTypes.number.isRequired
+};
 
 const ClockHand = ({ angle, scale }) => {
   return (
     <div className="time-picker-clock__hand" style={{ transform: `rotate(${angle}deg) scaleY(${scale})`}}></div>
   );
 }
+
+ClockHand.propTypes = propTypes;
 
 export default ClockHand;
