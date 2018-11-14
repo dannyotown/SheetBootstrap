335a7159691ef017e6ec6091ac4a533a41f6d23d
Anna Morawska <a.morawska@mdbootstrap.com>
2018-11-14 14:28:30 +0100

    Add new demonstration page for bootstrap alerts

---
+++ docs/pages/AlertPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from 'mdbreact';
+import DocsLink from "./DocsLink";
+
+const AlertPage = () => {
+
+  return (
+    <Container>
+      <DocsLink
+        title="Alerts"
+        href="https://mdbootstrap.com/docs/react/advanced/alerts/"
+      />
+
+    </Container>
+  );
+};
+
+export default AlertPage;
