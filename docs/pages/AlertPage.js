c7f1d711d1bc361ed4e67e5e4a453256e42bb831
Anna Morawska <a.morawska@mdbootstrap.com>
2018-11-15 15:35:13 +0100

    Rename components to assure consistency with new syntax with MDB prefix.

--- docs/pages/AlertPage.js
+++ docs/pages/AlertPage.js
@@ -1,77 +1,74 @@
 import React from "react";
-import { Container, Alert } from 'mdbreact';
+import { MDBContainer, MDBAlert } from 'mdbreact';
 import DocsLink from "./DocsLink";
 
 const AlertPage = () => {
-
-
-
   return (
-    <Container>
+    <MDBContainer>
       <DocsLink
         title="Alerts"
         href="https://mdbootstrap.com/docs/react/advanced/alerts/"
       />
-      <Alert color="success">
+      <MDBAlert color="success">
         <h4 className="alert-heading">Well done!</h4>
         <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
         <hr />
         <p className="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-      </Alert>
+      </MDBAlert>
 
-      <Alert color="danger" dismiss onClose={()=> alert('This event fires immediately when the close instance method is called.')} onClosed={()=> alert('This event is fired when the alert has been closed (will wait for CSS transitions to complete).')}>
+      <MDBAlert color="danger" dismiss onClose={()=> alert('This event fires immediately when the close instance method is called.')} onClosed={()=> alert('This event is fired when the alert has been closed (will wait for CSS transitions to complete).')}>
         Alert component exposes a few events for hooking into alert functionality.
-      </Alert>
-      <Alert color="primary" dismiss>
+      </MDBAlert>
+      <MDBAlert color="primary" dismiss>
         A simple primary alert—check it out!
-      </Alert>
-      <Alert color="secondary" dismiss>
+      </MDBAlert>
+      <MDBAlert color="secondary" dismiss>
         A simple secondary alert—check it out!
-      </Alert>
-      <Alert color="success" dismiss>
+      </MDBAlert>
+      <MDBAlert color="success" dismiss>
         A simple success alert—check it out!
-      </Alert>
-      <Alert color="danger" dismiss>
+      </MDBAlert>
+      <MDBAlert color="danger" dismiss>
         A simple success alert—check it out!
-      </Alert>
-      <Alert color="warning" dismiss>
+      </MDBAlert>
+      <MDBAlert color="warning" dismiss>
         A simple warning alert—check it out!
-      </Alert>
-      <Alert color="info" dismiss>
+      </MDBAlert>
+      <MDBAlert color="info" dismiss>
         A simple info alert—check it out!
-      </Alert>
-      <Alert color="light" dismiss>
+      </MDBAlert>
+      <MDBAlert color="light" dismiss>
         A simple light alert—check it out!
-      </Alert>
-      <Alert color="dark" dismiss>
+      </MDBAlert>
+      <MDBAlert color="dark" dismiss>
         A simple dark alert—check it out!
-      </Alert>
+      </MDBAlert>
 
-      <Alert color="primary">
+      <MDBAlert color="primary">
         A simple primary alert with <a href="#!" className="alert-link">an example link</a>. Give it a click if you like.
-      </Alert>
-      <Alert color="secondary">
+      </MDBAlert>
+      <MDBAlert color="secondary">
         A simple primary alert with <a href="#!" className="alert-link">an example link</a>. Give it a click if you like.
-      </Alert>
-      <Alert color="success">
+      </MDBAlert>
+      <MDBAlert color="success">
         A simple primary alert with <a href="#!" className="alert-link">an example link</a>. Give it a click if you like.
-      </Alert>
-      <Alert color="danger">
+      </MDBAlert>
+      <MDBAlert color="danger">
         A simple primary alert with <a href="#!" className="alert-link">an example link</a>. Give it a click if you like.
-      </Alert>
-      <Alert color="warning">
+      </MDBAlert>
+      <MDBAlert color="warning">
         A simple primary alert with <a href="#!" className="alert-link">an example link</a>. Give it a click if you like.
-      </Alert>
-      <Alert color="info">
+      </MDBAlert>
+      <MDBAlert color="info">
         A simple primary alert with <a href="#!" className="alert-link">an example link</a>. Give it a click if you like.
-      </Alert>
-      <Alert color="light">
+      </MDBAlert>
+      <MDBAlert color="light">
         A simple primary alert with <a href="#!" className="alert-link">an example link</a>. Give it a click if you like.
-      </Alert>
-      <Alert color="dark">
+      </MDBAlert>
+      <MDBAlert color="dark">
         A simple primary alert with <a href="#!" className="alert-link">an example link</a>. Give it a click if you like.
-      </Alert>
-    </Container>
+      </MDBAlert>
+    </MDBContainer>
   );
 };
 
