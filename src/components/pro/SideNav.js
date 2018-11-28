e6883837f4153675781b12dbed5973a9d7fd8157
Jakub Mandra <j.mandra@mdbootstrap.com>
2018-11-28 15:52:42 +0100

    Tidy up in SideNav properties

--- src/components/pro/SideNav.js
+++ src/components/pro/SideNav.js
@@ -78,7 +78,6 @@ class SideNav extends React.Component {
       hidden,
       right,
       triggerOpening,
-      key,
       onOverlayClick,
       mask,
       ...attributes
@@ -96,7 +95,6 @@ class SideNav extends React.Component {
       <div
         id="sidenav-overlay"
         onClick={this.handleOverlayClick}
-        key="overlay"
       />
     );
 
@@ -105,7 +103,6 @@ class SideNav extends React.Component {
         {...attributes}
         className={classes}
         style={{ backgroundImage: `url(${bg}` }}
-        key="{key}"
       >
         <ul className="custom-scrollbar list-unstyled">
           {logo && (
@@ -148,21 +145,28 @@ class SideNav extends React.Component {
 }
 
 SideNav.propTypes = {
-  children: PropTypes.node,
-  tag: PropTypes.string,
-  className: PropTypes.string,
-  href: PropTypes.string,
-  src: PropTypes.string,
-  breakWidth: PropTypes.number,
-  triggerOpening: PropTypes.bool,
   bg: PropTypes.string,
-  mask: PropTypes.string
+  breakWidth: PropTypes.number,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  hidden: PropTypes.bool,
+  href: PropTypes.string,
+  logo: PropTypes.string,
+  mask: PropTypes.string,
+  triggerOpening: PropTypes.bool,
+  tag: PropTypes.string
 };
 
 SideNav.defaultProps = {
-  tag: "div",
+  bg: '',
+  breakWidth: 1400,
+  className: '',
+  hidden: false,
   href: "#",
-  breakWidth: 1400
+  logo: '',
+  mask: '',
+  triggerOpening: false,
+  tag: "div"
 };
 
 export default SideNav;
