a24e1a3e6de015901ddc56aae74a40b2a3026533
Anna Morawska <a.morawska@mdbootstrap.com>
2018-12-17 14:08:15 +0100

    Create boilerplate for new  StreakPage

---
+++ docs/pages/pro/StreakPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const StreakPage = () => {
+  return (
+    <div className="streak streak-md streak-photo" style="background-image:url('https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img(115).jpg')">
+      <div className="flex-center white-text rgba-black-light pattern-1">
+        <ul className="mb-0 list-unstyled">
+          <li>
+            <h2 className="h2-responsive"><i className="fa fa-quote-left" aria-hidden="true"></i> The people who are
+          crazy enough to think they can change the world are the ones who do. <i className="fa fa-quote-right"
+                aria-hidden="true"></i></h2>
+          </li>
+          <li className="mb-0">
+            <h5 className="text-center font-italic mb-0">~ Steve Jobs</h5>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default StreakPage;
+
+
