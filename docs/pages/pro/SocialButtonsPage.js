50b2d41c7be5d9e362b8a9102b8ee610b563a267
Jakub Mandra <j.mandra@mdbootstrap.com>
2018-12-24 14:31:24 +0100

    Update icons on SocialButtonsPage

--- docs/pages/pro/SocialButtonsPage.js
+++ docs/pages/pro/SocialButtonsPage.js
@@ -12,37 +12,37 @@ const SocialButtonsPage = props => {
       <Container>
         <h4 className="mt-4">Full name social button</h4>
         <Button social="fb">
-          <Fa icon="facebook" className="pr-1" /> Facebook
+          <Fa brand icon="facebook" className="pr-1" /> Facebook
         </Button>
         <Button social="tw">
-          <Fa icon="twitter" className="pr-1" /> Twitter
+          <Fa brand icon="twitter" className="pr-1" /> Twitter
         </Button>
         <Button social="gplus">
-          <Fa icon="google-plus" className="pr-1" /> Google +
+          <Fa brand icon="google-plus" className="pr-1" /> Google +
         </Button>
         <Button social="li">
-          <Fa icon="linkedin" className="pr-1" /> Linkedin
+          <Fa brand icon="linkedin" className="pr-1" /> Linkedin
         </Button>
         <Button social="ins">
-          <Fa icon="instagram" className="pr-1" /> Instagram
+          <Fa brand icon="instagram" className="pr-1" /> Instagram
         </Button>
         <Button social="pin">
-          <Fa icon="pinterest" className="pr-1" /> Pinterest
+          <Fa brand icon="pinterest" className="pr-1" /> Pinterest
         </Button>
         <Button social="yt">
-          <Fa icon="youtube" className="pr-1" /> Youtube
+          <Fa brand icon="youtube" className="pr-1" /> Youtube
         </Button>
         <Button social="vk">
-          <Fa icon="vk" className="pr-1" /> Vkontakte
+          <Fa brand icon="vk" className="pr-1" /> Vkontakte
         </Button>
         <Button social="so">
-          <Fa icon="stack-overflow" className="pr-1" /> Stack Overflow
+          <Fa brand icon="stack-overflow" className="pr-1" /> Stack Overflow
         </Button>
         <Button social="slack">
-          <Fa icon="slack" className="pr-1" /> Slack
+          <Fa brand icon="slack" className="pr-1" /> Slack
         </Button>
         <Button social="git">
-          <Fa icon="github" className="pr-1" /> Github
+          <Fa brand icon="github" className="pr-1" /> Github
         </Button>
         <Button social="comm">
           <Fa icon="comments" className="pr-1" /> Comments
@@ -51,45 +51,45 @@ const SocialButtonsPage = props => {
           <Fa icon="envelope" className="pr-1" /> Email
         </Button>
         <Button social="dribbble">
-          <Fa icon="dribbble" className="pr-1" /> Dribbble
+          <Fa brand icon="dribbble" className="pr-1" /> Dribbble
         </Button>
         <h4 className="mt-4">Large button</h4>
         <Button social="fb" size="lg">
-          <Fa icon="facebook" className="pr-1" /> Facebook
+          <Fa brand icon="facebook" className="pr-1" /> Facebook
         </Button>
         <h4 className="mt-4">Simple social button</h4>
         <Button social="fb">
-          <Fa icon="facebook" />
+          <Fa brand icon="facebook" />
         </Button>
         <Button social="tw">
-          <Fa icon="twitter" />
+          <Fa brand icon="twitter" />
         </Button>
         <Button social="gplus">
-          <Fa icon="google-plus" />
+          <Fa brand icon="google-plus" />
         </Button>
         <Button social="li">
-          <Fa icon="linkedin" />
+          <Fa brand icon="linkedin" />
         </Button>
         <Button social="ins">
-          <Fa icon="instagram" />
+          <Fa brand icon="instagram" />
         </Button>
         <Button social="pin">
-          <Fa icon="pinterest" />
+          <Fa brand icon="pinterest" />
         </Button>
         <Button social="yt">
-          <Fa icon="youtube" />
+          <Fa brand icon="youtube" />
         </Button>
         <Button social="vk">
-          <Fa icon="vk" />
+          <Fa brand icon="vk" />
         </Button>
         <Button social="so">
-          <Fa icon="stack-overflow" />
+          <Fa brand icon="stack-overflow" />
         </Button>
         <Button social="slack">
-          <Fa icon="slack" />
+          <Fa brand icon="slack" />
         </Button>
         <Button social="git">
-          <Fa icon="github" />
+          <Fa brand icon="github" />
         </Button>
         <Button social="comm">
           <Fa icon="comments" />
@@ -98,45 +98,45 @@ const SocialButtonsPage = props => {
           <Fa icon="envelope" />
         </Button>
         <Button social="dribbble">
-          <Fa icon="dribbble" />
+          <Fa brand icon="dribbble" />
         </Button>
         <h4 className="mt-4">Large button</h4>
         <Button social="fb" size="lg">
-          <Fa icon="facebook" />
+          <Fa brand icon="facebook" />
         </Button>
         <h4 className="mt-4">Floating social buttons</h4>
         <Button size="lg" tag="a" floating social="fb">
-          <Fa icon="facebook" />
+          <Fa brand icon="facebook" />
         </Button>
         <Button size="lg" tag="a" floating social="tw">
-          <Fa icon="twitter" />
+          <Fa brand icon="twitter" />
         </Button>
         <Button size="lg" tag="a" floating social="gplus">
-          <Fa icon="google-plus" />
+          <Fa brand icon="google-plus" />
         </Button>
         <Button size="lg" tag="a" floating social="li">
-          <Fa icon="linkedin" />
+          <Fa brand icon="linkedin" />
         </Button>
         <Button size="lg" tag="a" floating social="ins">
-          <Fa icon="instagram" />
+          <Fa brand icon="instagram" />
         </Button>
         <Button size="lg" tag="a" floating social="pin">
-          <Fa icon="pinterest" />
+          <Fa brand icon="pinterest" />
         </Button>
         <Button size="lg" tag="a" floating social="yt">
-          <Fa icon="youtube" />
+          <Fa brand icon="youtube" />
         </Button>
         <Button size="lg" tag="a" floating social="vk">
-          <Fa icon="vk" />
+          <Fa brand icon="vk" />
         </Button>
         <Button size="lg" tag="a" floating social="so">
-          <Fa icon="stack-overflow" />
+          <Fa brand icon="stack-overflow" />
         </Button>
         <Button size="lg" tag="a" floating social="slack">
-          <Fa icon="slack" />
+          <Fa brand icon="slack" />
         </Button>
         <Button size="lg" tag="a" floating social="git">
-          <Fa icon="github" />
+          <Fa brand icon="github" />
         </Button>
         <Button size="lg" tag="a" floating social="comm">
           <Fa icon="comments" />
@@ -145,61 +145,61 @@ const SocialButtonsPage = props => {
           <Fa icon="envelope" />
         </Button>
         <Button size="lg" tag="a" floating social="dribbble">
-          <Fa icon="dribbble" />
+          <Fa brand icon="dribbble" />
         </Button>
         <h4 className="mt-4">Sizes</h4>
         <Button size="lg" tag="a" floating social="fb">
-          <Fa icon="facebook" />
+          <Fa brand icon="facebook" />
         </Button>
         <Button tag="a" floating social="fb">
-          <Fa icon="facebook" />
+          <Fa brand icon="facebook" />
         </Button>
         <Button size="sm" tag="a" floating social="fb">
-          <Fa icon="facebook" />
+          <Fa brand icon="facebook" />
         </Button>
         <h4 className="mt-4">Full name social counters</h4>
         <Button social="fb">
-          <Fa icon="facebook" className="pr-1" /> Facebook
+          <Fa brand icon="facebook" className="pr-1" /> Facebook
         </Button>
         <span className="counter">22</span>
         <Button social="tw">
-          <Fa icon="twitter" className="pr-1" /> Twitter
+          <Fa brand icon="twitter" className="pr-1" /> Twitter
         </Button>
         <span className="counter">22</span>
         <Button social="gplus">
-          <Fa icon="google-plus" className="pr-1" /> Google +
+          <Fa brand icon="google-plus" className="pr-1" /> Google +
         </Button>
         <span className="counter">22</span>
         <Button social="li">
-          <Fa icon="linkedin" className="pr-1" /> Linkedin
+          <Fa brand icon="linkedin" className="pr-1" /> Linkedin
         </Button>
         <span className="counter">22</span>
         <Button social="ins">
-          <Fa icon="instagram" className="pr-1" /> Instagram
+          <Fa brand icon="instagram" className="pr-1" /> Instagram
         </Button>
         <span className="counter">22</span>
         <Button social="pin">
-          <Fa icon="pinterest" className="pr-1" /> Pinterest
+          <Fa brand icon="pinterest" className="pr-1" /> Pinterest
         </Button>
         <span className="counter">22</span>
         <Button social="yt">
-          <Fa icon="youtube" className="pr-1" /> Youtube
+          <Fa brand icon="youtube" className="pr-1" /> Youtube
         </Button>
         <span className="counter">22</span>
         <Button social="vk">
-          <Fa icon="vk" className="pr-1" /> Vkontakte
+          <Fa brand icon="vk" className="pr-1" /> Vkontakte
         </Button>
         <span className="counter">22</span>
         <Button social="so">
-          <Fa icon="stack-overflow" className="pr-1" /> Stack Overflow
+          <Fa brand icon="stack-overflow" className="pr-1" /> Stack Overflow
         </Button>
         <span className="counter">22</span>
         <Button social="slack">
-          <Fa icon="slack" className="pr-1" /> Slack
+          <Fa brand icon="slack" className="pr-1" /> Slack
         </Button>
         <span className="counter">22</span>
         <Button social="git">
-          <Fa icon="github" className="pr-1" /> Github
+          <Fa brand icon="github" className="pr-1" /> Github
         </Button>
         <span className="counter">22</span>
         <Button social="comm">
@@ -211,52 +211,52 @@ const SocialButtonsPage = props => {
         </Button>
         <span className="counter">22</span>
         <Button social="dribbble">
-          <Fa icon="dribbble" className="pr-1" /> Dribbble
+          <Fa brand icon="dribbble" className="pr-1" /> Dribbble
         </Button>
         <span className="counter">22</span>
         <h4 className="mt-4">Simple social counters </h4>
         <Button size="lg" social="fb">
-          <Fa icon="facebook" />
+          <Fa brand icon="facebook" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="tw">
-          <Fa icon="twitter" />
+          <Fa brand icon="twitter" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="gplus">
-          <Fa icon="google-plus" />
+          <Fa brand icon="google-plus" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="li">
-          <Fa icon="linkedin" />
+          <Fa brand icon="linkedin" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="ins">
-          <Fa icon="instagram" />
+          <Fa brand icon="instagram" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="pin">
-          <Fa icon="pinterest" />
+          <Fa brand icon="pinterest" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="yt">
-          <Fa icon="youtube" />
+          <Fa brand icon="youtube" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="vk">
-          <Fa icon="vk" />
+          <Fa brand icon="vk" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="so">
-          <Fa icon="stack-overflow" />
+          <Fa brand icon="stack-overflow" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="slack">
-          <Fa icon="slack" />
+          <Fa brand icon="slack" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="git">
-          <Fa icon="github" />
+          <Fa brand icon="github" />
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="comm">
@@ -268,51 +268,51 @@ const SocialButtonsPage = props => {
         </Button>
         <span className="counter">22</span>
         <Button size="lg" social="dribbble">
-          <Fa icon="dribbble" />
+          <Fa brand icon="dribbble" />
         </Button>
         <span className="counter">22</span>
-        <h4 className="mt-4">Social Icons</h4>
+        <h4 className="mt-4">Social brand icons</h4>
         <a href="#!" className="fb-ic mr-3">
-          <Fa icon="facebook" />
+          <Fa brand icon="facebook" />
         </a>
         <a href="#!" className="tw-ic mr-3">
-          <Fa icon="twitter" />
+          <Fa brand icon="twitter" />
         </a>
         <a href="#!" className="gplus-ic mr-3">
-          <Fa icon="google-plus" />
+          <Fa brand icon="google-plus" />
         </a>
         <a href="#!" className="li-ic mr-3">
-          <Fa icon="linkedin" />
+          <Fa brand icon="linkedin" />
         </a>
         <a href="#!" className="ins-ic mr-3">
-          <Fa icon="instagram" />
+          <Fa brand icon="instagram" />
         </a>
         <a href="#!" className="pin-ic mr-3">
-          <Fa icon="pinterest" />
+          <Fa brand icon="pinterest" />
         </a>
         <a href="#!" className="yt-ic mr-3">
-          <Fa icon="youtube" />
+          <Fa brand icon="youtube" />
         </a>
         <a href="#!" className="vk-ic mr-3">
-          <Fa icon="vk" />
+          <Fa brand icon="vk" />
         </a>
         <a href="#!" className="so-ic mr-3">
-          <Fa icon="stack-overflow" />
+          <Fa brand icon="stack-overflow" />
         </a>
         <a href="#!" className="slack-ic mr-3">
-          <Fa icon="slack" />
+          <Fa brand icon="slack" />
         </a>
         <a href="#!" className="git-ic mr-3">
-          <Fa icon="github" />
+          <Fa brand icon="github" />
         </a>
         <a href="#!" className="comm-ic mr-3">
           <Fa icon="comments" />
         </a>
         <a href="#!" className="email-ic mr-3">
-          <Fa icon="envelope-o" />
+          <Fa icon="envelope" />
         </a>
         <a href="#!" className="dribbble-ic mr-3">
-          <Fa icon="dribbble" />
+          <Fa brand icon="dribbble" />
         </a>
       </Container>
     </Container>
