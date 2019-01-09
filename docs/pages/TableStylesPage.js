b5ae278446a88a4c9b8f975b61bbb6e2a638ac6f
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-01-09 10:23:54 +0100

    Update icons and prefixes on TableStylesPage

--- docs/pages/TableStylesPage.js
+++ docs/pages/TableStylesPage.js
@@ -1,16 +1,17 @@
 import React from "react";
 import {
-  Button,
-  Container,
-  Row,
-  Col,
-  Card,
-  CardBody,
-  CardHeader,
-  Input,
-  Table,
-  TableBody,
-  TableHead
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardHeader,
+  MDBInput,
+  MDBTable,
+  MDBTableBody,
+  MDBTableHead,
+  MDBIcon
 } from "mdbreact";
 import DocsLink from "./DocsLink";
 
@@ -42,9 +43,9 @@ const TablePage = props => {
     {
       id: 1,
       first: (
-        <Button color="purple" size="sm">
+        <MDBBtn color="purple" size="sm">
           Button
-        </Button>
+        </MDBBtn>
       ),
       last: "Otto",
       handle: "@mdo"
@@ -53,9 +54,9 @@ const TablePage = props => {
       id: 2,
       first: "Jacob",
       last: (
-        <Button color="purple" size="sm">
+        <MDBBtn color="purple" size="sm">
           Button
-        </Button>
+        </MDBBtn>
       ),
       handle: "@fat"
     },
@@ -64,9 +65,9 @@ const TablePage = props => {
       first: "Larry",
       last: "the Bird",
       handle: (
-        <Button color="purple" size="sm">
+        <MDBBtn color="purple" size="sm">
           Button
-        </Button>
+        </MDBBtn>
       )
     }
   ];
@@ -75,9 +76,9 @@ const TablePage = props => {
     {
       id: 1,
       first: (
-        <Button color="purple" outline size="sm">
+        <MDBBtn color="purple" outline size="sm">
           Button
-        </Button>
+        </MDBBtn>
       ),
       last: "Otto",
       handle: "@mdo"
@@ -86,9 +87,9 @@ const TablePage = props => {
       id: 2,
       first: "Jacob",
       last: (
-        <Button color="purple" outline size="sm">
+        <MDBBtn color="purple" outline size="sm">
           Button
-        </Button>
+        </MDBBtn>
       ),
       handle: "@fat"
     },
@@ -97,9 +98,9 @@ const TablePage = props => {
       first: "Larry",
       last: "the Bird",
       handle: (
-        <Button color="purple" outline size="sm">
+        <MDBBtn color="purple" outline size="sm">
           Button
-        </Button>
+        </MDBBtn>
       )
     }
   ];
@@ -108,9 +109,9 @@ const TablePage = props => {
     {
       id: 1,
       first: (
-        <Button color="default" rounded size="sm">
+        <MDBBtn color="default" rounded size="sm">
           Button
-        </Button>
+        </MDBBtn>
       ),
       last: "Otto",
       handle: "@mdo"
@@ -119,9 +120,9 @@ const TablePage = props => {
       id: 2,
       first: "Jacob",
       last: (
-        <Button color="default" rounded size="sm">
+        <MDBBtn color="default" rounded size="sm">
           Button
-        </Button>
+        </MDBBtn>
       ),
       handle: "@fat"
     },
@@ -130,9 +131,9 @@ const TablePage = props => {
       first: "Larry",
       last: "the Bird",
       handle: (
-        <Button color="default" rounded size="sm">
+        <MDBBtn color="default" rounded size="sm">
           Button
-        </Button>
+        </MDBBtn>
       )
     }
   ];
@@ -162,19 +163,19 @@ const TablePage = props => {
     ],
     rows: [
       {
-        check: <Input label="Check 2" type="checkbox" id="checkbox2" />,
+        check: <MDBInput label="Check 2" type="checkbox" id="checkbox2" />,
         lorem: "Cell1",
         ipsum: "Cell2",
         dolor: "Cell3"
       },
       {
-        check: <Input label="Check 3" type="checkbox" id="checkbox3" />,
+        check: <MDBInput label="Check 3" type="checkbox" id="checkbox3" />,
         lorem: "Cell1",
         ipsum: "Cell2",
         dolor: "Cell3"
       },
       {
-        check: <Input label="Check 4" type="checkbox" id="checkbox4" />,
+        check: <MDBInput label="Check 4" type="checkbox" id="checkbox4" />,
         lorem: "Cell1",
         ipsum: "Cell2",
         dolor: "Cell3"
@@ -191,9 +192,10 @@ const TablePage = props => {
       },
       {
         label: [
-          <i
+          <MDBIcon
             key="Lorem"
-            className="fa fa-leaf mr-2 blue-text"
+            icon="leaf"
+            className="mr-2 blue-text"
             aria-hidden="true"
           />,
           "Lorem"
@@ -203,9 +205,10 @@ const TablePage = props => {
       },
       {
         label: [
-          <i
+          <MDBIcon
             key="Ipsum"
-            className="fa fa-leaf mr-2 teal-text"
+            icon="leaf"
+            className="mr-2 teal-text"
             aria-hidden="true"
           />,
           "Ipsum"
@@ -215,9 +218,10 @@ const TablePage = props => {
       },
       {
         label: [
-          <i
+          <MDBIcon
             key="Dolor"
-            className="fa fa-leaf mr-2 indigo-text"
+            icon="leaf"
+            className="mr-2 indigo-text"
             aria-hidden="true"
           />,
           "Dolor"
@@ -230,25 +234,28 @@ const TablePage = props => {
       {
         id: 1,
         lorem: [
-          <i
+          <MDBIcon
             key="cell1"
-            className="fa fa-diamond mr-2 grey-text"
+            icon="gem"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell1"
         ],
         ipsum: [
-          <i
+          <MDBIcon
             key="cell2"
-            className="fa fa-download mr-2 grey-text"
+            icon="download"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell2"
         ],
         dolor: [
-          <i
+          <MDBIcon
             key="cell3"
-            className="fa fa-book mr-2 grey-text"
+            icon="book"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell3"
@@ -257,25 +264,28 @@ const TablePage = props => {
       {
         id: 2,
         lorem: [
-          <i
+          <MDBIcon
             key="cell1"
-            className="fa fa-graduation-cap mr-2 grey-text"
+            icon="graduation-cap"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell1"
         ],
         ipsum: [
-          <i
+          <MDBIcon
             key="cell2"
-            className="fa fa-gift mr-2 grey-text"
+            icon="gift"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell2"
         ],
         dolor: [
-          <i
+          <MDBIcon
             key="cell3"
-            className="fa fa-image mr-2 grey-text"
+            icon="image"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell3"
@@ -284,25 +294,28 @@ const TablePage = props => {
       {
         id: 3,
         lorem: [
-          <i
+          <MDBIcon
             key="cell1"
-            className="fa fa-magic mr-2 grey-text"
+            icon="magic"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell1"
         ],
         ipsum: [
-          <i
+          <MDBIcon
             key="cell2"
-            className="fa fa-table mr-2 grey-text"
+            icon="table"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell2"
         ],
         dolor: [
-          <i
+          <MDBIcon
             key="cell3"
-            className="fa fa-edit mr-2 grey-text"
+            icon="edit"
+            className="mr-2 grey-text"
             aria-hidden="true"
           />,
           "Cell3"
@@ -314,7 +327,7 @@ const TablePage = props => {
   const data_panel = {
     columns: [
       {
-        label: <Input label=" " type="checkbox" id="checkbox5" />,
+        label: <MDBInput label=" " type="checkbox" id="checkbox5" />,
         field: "check",
         sort: "asc"
       },
@@ -351,7 +364,7 @@ const TablePage = props => {
     ],
     rows: [
       {
-        check: <Input label=" " type="checkbox" id="checkbox6" />,
+        check: <MDBInput label=" " type="checkbox" id="checkbox6" />,
         first: "Mark",
         last: "Otto",
         username: "@mdo",
@@ -360,7 +373,7 @@ const TablePage = props => {
         username4: "@mdo"
       },
       {
-        check: <Input label=" " type="checkbox" id="checkbox7" />,
+        check: <MDBInput label=" " type="checkbox" id="checkbox7" />,
         first: "Jacob",
         last: "Thornton",
         username: "@fat",
@@ -369,7 +382,7 @@ const TablePage = props => {
         username4: "@fat"
       },
       {
-        check: <Input label=" " type="checkbox" id="checkbox8" />,
+        check: <MDBInput label=" " type="checkbox" id="checkbox8" />,
         first: "Larry",
         last: "the Bird",
         username: "@twitter",
@@ -378,7 +391,7 @@ const TablePage = props => {
         username4: "@twitter"
       },
       {
-        check: <Input label=" " type="checkbox" id="checkbox9" />,
+        check: <MDBInput label=" " type="checkbox" id="checkbox9" />,
         first: "Paul",
         last: "Topolski",
         username: "@P_Topolski",
@@ -387,7 +400,7 @@ const TablePage = props => {
         username4: "@P_Topolski"
       },
       {
-        check: <Input label=" " type="checkbox" id="checkbox10" />,
+        check: <MDBInput label=" " type="checkbox" id="checkbox10" />,
         first: "Larry",
         last: "the Bird",
         username: "@twitter",
@@ -399,148 +412,148 @@ const TablePage = props => {
   };
 
   return (
-    <Container className="mt-3">
+    <MDBContainer className="mt-3">
       <DocsLink
         title="Tables with additional elements"
         href="https://mdbootstrap.com/docs/react/tables/additional/"
       />
-      <Row className="py-3">
-        <Col md="12">
-          <Card>
-            <CardBody>
+      <MDBRow className="py-3">
+        <MDBCol md="12">
+          <MDBCard>
+            <MDBCardBody>
               <h2 className="h2-responsive pb-4">Table with regular buttons</h2>
-              <Table btn>
-                <TableHead columns={columns} />
-                <TableBody rows={rows_regular_btn} />
-              </Table>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+              <MDBTable btn>
+                <MDBTableHead columns={columns} />
+                <MDBTableBody rows={rows_regular_btn} />
+              </MDBTable>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
 
-      <Row className="py-3">
-        <Col md="12">
-          <Card>
-            <CardBody>
+      <MDBRow className="py-3">
+        <MDBCol md="12">
+          <MDBCard>
+            <MDBCardBody>
               <h2 className="h2-responsive pb-4">Table with outline buttons</h2>
-              <Table btn>
-                <TableHead columns={columns} />
-                <TableBody rows={rows_outline_btn} />
-              </Table>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+              <MDBTable btn>
+                <MDBTableHead columns={columns} />
+                <MDBTableBody rows={rows_outline_btn} />
+              </MDBTable>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
 
-      <Row className="py-3">
-        <Col md="12">
-          <Card>
-            <CardBody>
+      <MDBRow className="py-3">
+        <MDBCol md="12">
+          <MDBCard>
+            <MDBCardBody>
               <h2 className="h2-responsive pb-4">Table with outline buttons</h2>
-              <Table btn>
-                <TableHead columns={columns} />
-                <TableBody rows={rows_rounded_btn} />
-              </Table>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+              <MDBTable btn>
+                <MDBTableHead columns={columns} />
+                <MDBTableBody rows={rows_rounded_btn} />
+              </MDBTable>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
 
-      <Row className="py-3">
-        <Col md="12">
-          <Card>
-            <CardBody>
+      <MDBRow className="py-3">
+        <MDBCol md="12">
+          <MDBCard>
+            <MDBCardBody>
               <h2 className="h2-responsive pb-4">Table with checkboxes</h2>
-              <Table btn fixed>
-                <TableHead columns={data_checkboxes.columns} />
-                <TableBody rows={data_checkboxes.rows} />
-              </Table>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+              <MDBTable btn fixed>
+                <MDBTableHead columns={data_checkboxes.columns} />
+                <MDBTableBody rows={data_checkboxes.rows} />
+              </MDBTable>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
 
-      <Row className="py-3">
-        <Col md="12">
-          <Card>
-            <CardBody>
+      <MDBRow className="py-3">
+        <MDBCol md="12">
+          <MDBCard>
+            <MDBCardBody>
               <h2 className="h2-responsive pb-4">Table with icons</h2>
-              <Table btn fixed>
-                <TableHead columns={data_icons.columns} />
-                <TableBody rows={data_icons.rows} />
-              </Table>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
+              <MDBTable btn fixed>
+                <MDBTableHead columns={data_icons.columns} />
+                <MDBTableBody rows={data_icons.rows} />
+              </MDBTable>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
 
-      <Row className="py-3">
-        <Col md="12">
-          <Card narrow>
-            <CardHeader className="view view-cascade gradient-card-header blue-gradient d-flex justify-content-between align-items-center py-2 mx-4 mb-3">
+      <MDBRow className="py-3">
+        <MDBCol md="12">
+          <MDBCard narrow>
+            <MDBCardHeader className="view view-cascade gradient-card-header blue-gradient d-flex justify-content-between align-items-center py-2 mx-4 mb-3">
               <div>
-                <Button
+                <MDBBtn
                   outline
                   rounded
                   size="sm"
                   color="white"
                   className="px-2"
                 >
-                  <i className="fa fa-th-large mt-0" />
-                </Button>
-                <Button
+                  <MDBIcon icon="th-large"/>
+                </MDBBtn>
+                <MDBBtn
                   outline
                   rounded
                   size="sm"
                   color="white"
                   className="px-2"
                 >
-                  <i className="fa fa-columns mt-0" />
-                </Button>
+                  <MDBIcon icon="columns" />
+                </MDBBtn>
               </div>
               <a href="#!" className="white-text mx-3">
                 Table name
               </a>
               <div>
-                <Button
+                <MDBBtn
                   outline
                   rounded
                   size="sm"
                   color="white"
                   className="px-2"
                 >
-                  <i className="fa fa-pencil mt-0" />
-                </Button>
-                <Button
+                  <MDBIcon icon="pencil-alt" />
+                </MDBBtn>
+                <MDBBtn
                   outline
                   rounded
                   size="sm"
                   color="white"
                   className="px-2"
                 >
-                  <i className="fa fa-remove mt-0" />
-                </Button>
-                <Button
+                  <MDBIcon icon="times" />
+                </MDBBtn>
+                <MDBBtn
                   outline
                   rounded
                   size="sm"
                   color="white"
                   className="px-2"
                 >
-                  <i className="fa fa-info-circle mt-0" />
-                </Button>
+                  <MDBIcon icon="info-circle" />
+                </MDBBtn>
               </div>
-            </CardHeader>
-            <CardBody cascade>
-              <Table btn fixed>
-                <TableHead columns={data_panel.columns} />
-                <TableBody rows={data_panel.rows} />
-              </Table>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+            </MDBCardHeader>
+            <MDBCardBody cascade>
+              <MDBTable btn fixed>
+                <MDBTableHead columns={data_panel.columns} />
+                <MDBTableBody rows={data_panel.rows} />
+              </MDBTable>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
   );
 };
 
