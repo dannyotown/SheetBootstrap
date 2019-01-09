ef86ef0d6256defe58e4ad34213d675f41ee865d
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-01-09 11:16:04 +0100

    Update icons and prefixes in TimelinePage

--- docs/pages/pro/TimelinePage.js
+++ docs/pages/pro/TimelinePage.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { Container, Timeline, TimelineStep, Fa } from "mdbreact";
+import { MDBContainer, MDBTimeline, MDBTimelineStep, MDBIcon } from "mdbreact";
 import DocsLink from "../DocsLink";
 import "./TimelinePage.css";
 
 const TimelinePage = () => {
   return (
-    <Container>
+    <MDBContainer>
       <DocsLink
         title="Timeline"
         href="https://mdbootstrap.com/plugins/react/timeline/"
       />
-      <Container className="mt-5">
+      <MDBContainer className="mt-5">
         <section id="basic">
-          <Timeline>
-            <TimelineStep icon="check">
+          <MDBTimeline>
+            <MDBTimelineStep icon="check">
               <h4 className="font-weight-bold">Ut enim ad minim veniam</h4>
               <p className="text-muted mt-3">
-                <i className="fa fa-clock-o" aria-hidden="true" /> 11 hours ago
+                <i className="fa fa-clock" aria-hidden="true" /> 11 hours ago
                 via Twitter
               </p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
-            </TimelineStep>
-            <TimelineStep inverted color="warning-color" icon="credit-card-alt">
+            </MDBTimelineStep>
+            <MDBTimelineStep inverted color="warning-color" icon="credit-card">
               <h4 className="font-weight-bold">Duis aute irure dolor</h4>
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem
               accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
@@ -32,8 +32,8 @@ const TimelinePage = () => {
               dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
               aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
               eos qui ratione voluptatem sequi nesciunt.
-            </TimelineStep>
-            <TimelineStep icon="credit-card" color="danger-color">
+            </MDBTimelineStep>
+            <MDBTimelineStep icon="credit-card" color="danger-color">
               <h4 className="font-weight-bold">Sed ut nihil unde omnis</h4>
               Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
               consectetur, adipisci velit, sed quia non numquam eius modi
@@ -43,8 +43,8 @@ const TimelinePage = () => {
               consequatur? Quis autem vel eum iure reprehenderit qui in ea
               voluptate velit esse quam nihil molestiae consequatur, vel illum
               qui dolorem eum fugiat quo voluptas nulla pariatur?
-            </TimelineStep>
-            <TimelineStep inverted icon="floppy-o" color="info-color">
+            </MDBTimelineStep>
+            <MDBTimelineStep inverted icon="save" color="info-color">
               <h4 className="font-weight-bold">Quis autem vel eum voluptate</h4>
               At vero eos et accusamus et iusto odio dignissimos ducimus qui
               blanditiis praesentium voluptatum deleniti atque corrupti quos
@@ -52,8 +52,8 @@ const TimelinePage = () => {
               provident, similique sunt in culpa qui officia deserunt mollitia
               animi, id est laborum et dolorum fuga. Et harum quidem rerum
               facilis est et expedita distinctio.
-            </TimelineStep>
-            <TimelineStep icon="thumbs-o-up" color="success-color">
+            </MDBTimelineStep>
+            <MDBTimelineStep icon="thumbs-up" color="success-color">
               <h4 className="font-weight-bold">Mussum ipsum cacilds</h4>
               Nam libero tempore, cum soluta nobis est eligendi optio cumque
               nihil impedit quo minus id quod maxime placeat facere possimus,
@@ -63,20 +63,20 @@ const TimelinePage = () => {
               recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut
               aut reiciendis voluptatibus maiores alias consequatur aut
               perferendis doloribus asperiores repellat.
-            </TimelineStep>
-          </Timeline>
+            </MDBTimelineStep>
+          </MDBTimeline>
         </section>
 
         <hr className="my-5" />
 
         <section id="timelineColorful">
-          <Timeline>
-            <TimelineStep colorful icon="check" color="red darken-2">
+          <MDBTimeline>
+            <MDBTimelineStep colorful icon="check" color="red darken-2">
               <h4 className="font-weight-bold red darken-2 white-text p-3">
                 Ut enim ad minim veniam
               </h4>
               <p className="text-muted mt-3 px-4">
-                <i className="fa fa-clock-o" aria-hidden="true" /> 11 hours ago
+                <i className="fa fa-clock" aria-hidden="true" /> 11 hours ago
                 via Twitter
               </p>
               <p className="mb-0 px-4 pb-4">
@@ -85,18 +85,18 @@ const TimelinePage = () => {
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
               </p>
-            </TimelineStep>
-            <TimelineStep
+            </MDBTimelineStep>
+            <MDBTimelineStep
               colorful
               inverted
               color="red darken-2"
-              icon="credit-card-alt"
+              icon="credit-card"
             >
               <h4 className="font-weight-bold red darken-2 white-text p-3">
                 Ut enim ad minim veniam
               </h4>
               <p className="text-muted mt-4 px-4">
-                <i className="fa fa-clock-o" aria-hidden="true" /> 11 hours ago
+                <i className="fa fa-clock" aria-hidden="true" /> 11 hours ago
                 via Twitter
               </p>
               <p className="mb-0 px-4 pb-4">
@@ -105,8 +105,8 @@ const TimelinePage = () => {
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
               </p>
-            </TimelineStep>
-            <TimelineStep colorful icon="credit-card" color="red darken-2">
+            </MDBTimelineStep>
+            <MDBTimelineStep colorful icon="credit-card" color="red darken-2">
               <h4 className="font-weight-bold  red darken-2 white-text p-3 mb-0">
                 Duis aute irure dolor
               </h4>
@@ -119,18 +119,18 @@ const TimelinePage = () => {
                 consequuntur magni dolores eos qui ratione voluptatem sequi
                 nesciunt.
               </p>
-            </TimelineStep>
-            <TimelineStep
+            </MDBTimelineStep>
+            <MDBTimelineStep
               colorful
               inverted
-              icon="floppy-o"
+              icon="save"
               color="red darken-2"
             >
               <h4 className="font-weight-bold red darken-2 white-text p-3">
                 Ut enim ad minim veniam
               </h4>
               <p className="text-muted mt-4 px-4">
-                <i className="fa fa-clock-o" aria-hidden="true" /> 11 hours ago
+                <i className="fa fa-clock" aria-hidden="true" /> 11 hours ago
                 via Twitter
               </p>
               <p className="mb-0 px-4 pb-4">
@@ -139,8 +139,8 @@ const TimelinePage = () => {
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
               </p>
-            </TimelineStep>
-            <TimelineStep colorful icon="thumbs-o-up" color="red darken-2">
+            </MDBTimelineStep>
+            <MDBTimelineStep colorful icon="thumbs-up" color="red darken-2">
               <h4 className="font-weight-bold red darken-2 white-text p-3 mb-0">
                 Mussum ipsum cacilds
               </h4>
@@ -154,20 +154,20 @@ const TimelinePage = () => {
                 sapiente delectus, ut aut reiciendis voluptatibus maiores alias
                 consequatur aut perferendis doloribus asperiores repellat.
               </p>
-            </TimelineStep>
-          </Timeline>
+            </MDBTimelineStep>
+          </MDBTimeline>
         </section>
 
         <hr className="my-5" />
 
         <section id="timelineHoverable">
-          <Timeline>
-            <TimelineStep hoverable color="grey">
+          <MDBTimeline>
+            <MDBTimelineStep hoverable color="grey">
               <h4 className="font-weight-bold p-4 mb-0">
                 Ut enim ad minim veniam
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2017
+                <MDBIcon icon="clock" /> 2017
               </p>
               <p className="mb-0 p-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -175,13 +175,13 @@ const TimelinePage = () => {
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
               </p>
-            </TimelineStep>
-            <TimelineStep inverted hoverable color="grey">
+            </MDBTimelineStep>
+            <MDBTimelineStep inverted hoverable color="grey">
               <h4 className="font-weight-bold p-4 mb-0">
                 Duis aute irure dolor
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2016
+                <MDBIcon icon="clock" /> 2016
               </p>
               <p className="mb-0 p-4">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem
@@ -192,13 +192,13 @@ const TimelinePage = () => {
                 consequuntur magni dolores eos qui ratione voluptatem sequi
                 nesciunt.
               </p>
-            </TimelineStep>
-            <TimelineStep hoverable color="grey">
+            </MDBTimelineStep>
+            <MDBTimelineStep hoverable color="grey">
               <h4 className="font-weight-bold p-4 mb-0">
                 Sed ut nihil unde omnis
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2015
+                <MDBIcon icon="clock" /> 2015
               </p>
               <p className="mb-0 p-4">
                 Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
@@ -211,13 +211,13 @@ const TimelinePage = () => {
                 molestiae consequatur, vel illum qui dolorem eum fugiat quo
                 voluptas nulla pariatur?
               </p>
-            </TimelineStep>
-            <TimelineStep inverted hoverable color="grey">
+            </MDBTimelineStep>
+            <MDBTimelineStep inverted hoverable color="grey">
               <h4 className="font-weight-bold p-4 mb-0">
                 Quis autem vel eum voluptate
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2014
+                <MDBIcon icon="clock" /> 2014
               </p>
               <p className="mb-0 p-4">
                 At vero eos et accusamus et iusto odio dignissimos ducimus qui
@@ -227,13 +227,13 @@ const TimelinePage = () => {
                 mollitia animi, id est laborum et dolorum fuga. Et harum quidem
                 rerum facilis est et expedita distinctio.
               </p>
-            </TimelineStep>
-            <TimelineStep hoverable color="grey">
+            </MDBTimelineStep>
+            <MDBTimelineStep hoverable color="grey">
               <h4 className="font-weight-bold p-4 mb-0">
                 Mussum ipsum cacilds
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2013
+                <MDBIcon icon="clock" /> 2013
               </p>
               <p className="mb-0 p-4">
                 Temporibus autem quibusdam et aut officiis debitis aut rerum
@@ -242,15 +242,15 @@ const TimelinePage = () => {
                 sapiente delectus, ut aut reiciendis voluptatibus maiores alias
                 consequatur aut perferendis doloribus asperiores repellat.
               </p>
-            </TimelineStep>
-          </Timeline>
+            </MDBTimelineStep>
+          </MDBTimeline>
         </section>
 
         <hr className="my-5" />
 
         <section id="timelineImages">
-          <Timeline>
-            <TimelineStep colorful hoverable color="cyan" label="1">
+          <MDBTimeline>
+            <MDBTimelineStep colorful hoverable color="cyan" label="1">
               <img
                 src="https://mdbootstrap.com/img/Photos/Slides/img%20(135).jpg"
                 className="img-fluid"
@@ -260,7 +260,7 @@ const TimelinePage = () => {
                 Ut enim ad minim veniam
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2017
+                <MDBIcon icon="clock" /> 2017
               </p>
               <p className="mb-0 p-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -268,8 +268,8 @@ const TimelinePage = () => {
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
                 nisi ut aliquip ex ea commodo consequat.
               </p>
-            </TimelineStep>
-            <TimelineStep colorful inverted hoverable color="cyan" label="2">
+            </MDBTimelineStep>
+            <MDBTimelineStep colorful inverted hoverable color="cyan" label="2">
               <img
                 src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
                 className="img-fluid"
@@ -279,7 +279,7 @@ const TimelinePage = () => {
                 Duis aute irure dolor
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2016
+                <MDBIcon icon="clock" /> 2016
               </p>
               <p className="mb-0 p-4">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem
@@ -290,8 +290,8 @@ const TimelinePage = () => {
                 consequuntur magni dolores eos qui ratione voluptatem sequi
                 nesciunt.
               </p>
-            </TimelineStep>
-            <TimelineStep colorful hoverable color="cyan" label="3">
+            </MDBTimelineStep>
+            <MDBTimelineStep colorful hoverable color="cyan" label="3">
               <img
                 src="https://mdbootstrap.com/img/Photos/Slides/img%20(131).jpg"
                 className="img-fluid"
@@ -301,7 +301,7 @@ const TimelinePage = () => {
                 Sed ut nihil unde omnis
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2015
+                <MDBIcon icon="clock" /> 2015
               </p>
               <p className="mb-0 p-4">
                 Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
@@ -314,8 +314,8 @@ const TimelinePage = () => {
                 molestiae consequatur, vel illum qui dolorem eum fugiat quo
                 voluptas nulla pariatur?
               </p>
-            </TimelineStep>
-            <TimelineStep colorful inverted hoverable color="cyan" label="4">
+            </MDBTimelineStep>
+            <MDBTimelineStep colorful inverted hoverable color="cyan" label="4">
               <img
                 src="https://mdbootstrap.com/img/Photos/Slides/img%20(125).jpg"
                 className="img-fluid"
@@ -325,7 +325,7 @@ const TimelinePage = () => {
                 Quis autem vel eum voluptate
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2014
+                <MDBIcon icon="clock" /> 2014
               </p>
               <p className="mb-0 p-4">
                 At vero eos et accusamus et iusto odio dignissimos ducimus qui
@@ -335,8 +335,8 @@ const TimelinePage = () => {
                 mollitia animi, id est laborum et dolorum fuga. Et harum quidem
                 rerum facilis est et expedita distinctio.
               </p>
-            </TimelineStep>
-            <TimelineStep colorful hoverable color="cyan" label="5">
+            </MDBTimelineStep>
+            <MDBTimelineStep colorful hoverable color="cyan" label="5">
               <img
                 src="https://mdbootstrap.com/img/Photos/Slides/img%20(144).jpg"
                 className="img-fluid"
@@ -346,7 +346,7 @@ const TimelinePage = () => {
                 Mussum ipsum cacilds
               </h4>
               <p className="text-muted px-4 mb-0">
-                <Fa icon="clock-o" /> 2013
+                <MDBIcon icon="clock" /> 2013
               </p>
               <p className="mb-0 p-4">
                 Temporibus autem quibusdam et aut officiis debitis aut rerum
@@ -355,11 +355,11 @@ const TimelinePage = () => {
                 sapiente delectus, ut aut reiciendis voluptatibus maiores alias
                 consequatur aut perferendis doloribus asperiores repellat.
               </p>
-            </TimelineStep>
-          </Timeline>
+            </MDBTimelineStep>
+          </MDBTimeline>
         </section>
-      </Container>
-    </Container>
+      </MDBContainer>
+    </MDBContainer>
   );
 };
 
