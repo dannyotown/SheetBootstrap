4f4641d06201c2fb5d99b956b38fd5b1f02b450b
Anna Morawska <a.morawska@mdbootstrap.com>
2019-01-11 11:08:15 +0100

    Refractor CardPage

--- docs/pages/pro/CardsPage.js
+++ docs/pages/pro/CardsPage.js
@@ -1,163 +1,148 @@
-import React from "react";
-import {
-  Container,
-  Col,
-  Row,
-  Card,
-  CardUp,
-  CardBody,
-  CardImage,
-  CardTitle,
-  CardText,
-  Avatar,
-  FlippingCard,
-  Fa,
-  Button
-} from "mdbreact";
-import DocsLink from "../DocsLink";
+import React, { Component } from "react";
+import { MDBContainer, MDBCol, MDBRow, MDBCard, MDBCardUp, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBAvatar, MDBRotatingCard, MDBIcon, MDBBtn } from "mdbreact";
+import DocsLink from "../../components/docsLink";
 
-class FlippingCardPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { flipped: false };
-    this.handleFlipping = this.handleFlipping.bind(this);
+class CardPage extends Component {
+
+  state = {
+    flipped: false
   }
 
-  handleFlipping() {
+  handleFlipping = () => {
     this.setState({ flipped: !this.state.flipped });
   }
   render() {
     const colStyle = { maxWidth: "22rem" };
 
     return (
-      <Container>
+      <MDBContainer>
         <DocsLink
           title="Cards"
           href="https://mdbootstrap.com/docs/react/components/cards/"
         />
-        <Container>
+        <MDBContainer>
           <h2>Stylish cards</h2>
-          <Row className="mt-2">
-            <Col style={colStyle}>
-              <Card>
-                <CardImage
+          <MDBRow className="mt-2">
+            <MDBCol style={colStyle}>
+              <MDBCard>
+                <MDBCardImage
                   top
                   src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%28131%29.jpg"
                   overlay="white-slight"
                   hover
                   waves
-                  alt="Card image cap"
+                  alt="MDBCard image cap"
                 />
-                <CardBody>
+                <MDBCardBody>
                   <a
                     href="#!"
                     className="activator waves-effect waves-light mr-4"
                   >
-                    <Fa icon="share-alt" />
+                    <MDBIcon icon="share-alt" />
                   </a>
-                  <CardTitle>Card Title</CardTitle>
+                  <MDBCardTitle>MDBCard Title</MDBCardTitle>
                   <hr />
-                  <CardText>
+                  <MDBCardText>
                     Some quick example text to build on the card title and make
                     up the bulk of the card&apos;s content.
-                  </CardText>
+                  </MDBCardText>
                   <a
                     href="#!"
                     className="black-text d-flex justify-content-end"
                   >
                     <h5>
-                      Read more <Fa icon="angle-double-right" />
+                      Read more <MDBIcon icon="angle-double-right" />
                     </h5>
                   </a>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col style={colStyle}>
-              <Card>
-                <CardImage
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol style={colStyle}>
+              <MDBCard>
+                <MDBCardImage
                   top
                   src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2821%29.jpg"
                   overlay="white-slight"
                   hover
                   waves
-                  alt="Card image cap"
+                  alt="MDBCard image cap"
                 />
-                <CardBody className="elegant-color white-text rounded-bottom">
+                <MDBCardBody className="elegant-color white-text rounded-bottom">
                   <a
                     href="#!"
                     className="activator waves-effect waves-light mr-4"
                   >
-                    <Fa icon="share-alt" />
+                    <MDBIcon icon="share-alt" />
                   </a>
-                  <CardTitle>Card Title</CardTitle>
+                  <MDBCardTitle>MDBCard Title</MDBCardTitle>
                   <hr className="hr-light" />
-                  <CardText className="white-text">
+                  <MDBCardText className="white-text">
                     Some quick example text to build on the card title and make
                     up the bulk of the card&apos;s content.
-                  </CardText>
+                  </MDBCardText>
                   <a
                     href="#!"
                     className="black-text d-flex justify-content-end"
                   >
                     <h5 className="white-text">
-                      Read more <Fa icon="angle-double-right" />
+                      Read more <MDBIcon icon="angle-double-right" />
                     </h5>
                   </a>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
 
           <hr className="my-5" />
           <h2>Testimonial cards</h2>
 
-          <Row className="mt-2">
-            <Col style={colStyle}>
-              <Card testimonial>
-                <CardUp className="indigo lighten-1" />
-                <Avatar className="mx-auto white">
+          <MDBRow className="mt-2">
+            <MDBCol style={colStyle}>
+              <MDBCard testimonial>
+                <MDBCardUp className="indigo lighten-1" />
+                <MDBAvatar className="mx-auto white">
                   <img
                     src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg"
                     alt=""
                   />
-                </Avatar>
-                <CardBody>
+                </MDBAvatar>
+                <MDBCardBody>
                   <h4 className="card-title">Anna Doe</h4>
                   <hr />
                   <p>
-                    <Fa icon="quote-left" /> Lorem ipsum dolor sit amet,
+                    <MDBIcon icon="quote-left" /> Lorem ipsum dolor sit amet,
                     consectetur adipisicing elit. Eos, adipisci
                   </p>
-                </CardBody>
-              </Card>
-            </Col>
-            <Col style={colStyle}>
-              <Card testimonial>
-                <CardUp gradient="aqua" />
-                <Avatar className="mx-auto white">
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol style={colStyle}>
+              <MDBCard testimonial>
+                <MDBCardUp gradient="aqua" />
+                <MDBAvatar className="mx-auto white">
                   <img
                     src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2831%29.jpg"
                     alt=""
                   />
-                </Avatar>
-                <CardBody>
+                </MDBAvatar>
+                <MDBCardBody>
                   <h4 className="card-title">Martha Smith</h4>
                   <hr />
                   <p>
-                    <Fa icon="quote-left" /> Lorem ipsum dolor sit amet,
+                    <MDBIcon icon="quote-left" /> Lorem ipsum dolor sit amet,
                     consectetur adipisicing elit. Eos, adipisci
                   </p>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
 
           <hr className="my-5" />
           <h2>Image Overlay</h2>
 
-          <Row>
-            <Col>
-              <Card
+          <MDBRow>
+            <MDBCol>
+              <MDBCard
                 className="card-image"
                 style={{
                   backgroundImage:
@@ -167,26 +152,26 @@ class FlippingCardPage extends React.Component {
                 <div className="text-white text-center d-flex align-items-center rgba-black-strong py-5 px-4">
                   <div>
                     <h5 className="pink-text">
-                      <Fa icon="chart-pie" /> Marketing
+                      <MDBIcon icon="chart-pie" /> Marketing
                     </h5>
-                    <CardTitle tag="h3" className="pt-2">
+                    <MDBCardTitle tag="h3" className="pt-2">
                       <strong>This is card title</strong>
-                    </CardTitle>
+                    </MDBCardTitle>
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                       Repellat fugiat, laboriosam, voluptatem, optio vero odio
                       nam sit officia accusamus minus error nisi architecto
                       nulla ipsum dignissimos. Odit sed qui, dolorum!
                     </p>
-                    <Button color="pink">
-                      <Fa icon="clone left" /> View project
-                    </Button>
+                    <MDBBtn color="pink">
+                      <MDBIcon icon="clone left" /> View project
+                    </MDBBtn>
                   </div>
                 </div>
-              </Card>
-            </Col>
-            <Col>
-              <Card
+              </MDBCard>
+            </MDBCol>
+            <MDBCol>
+              <MDBCard
                 className="card-image"
                 style={{
                   backgroundImage:
@@ -196,52 +181,52 @@ class FlippingCardPage extends React.Component {
                 <div className="text-white text-center d-flex align-items-center rgba-indigo-strong py-5 px-4">
                   <div>
                     <h5 className="orange-text">
-                      <Fa icon="desktop" /> Software
+                      <MDBIcon icon="desktop" /> Software
                     </h5>
-                    <CardTitle tag="h3" className="pt-2">
+                    <MDBCardTitle tag="h3" className="pt-2">
                       <strong>This is card title</strong>
-                    </CardTitle>
+                    </MDBCardTitle>
                     <p>
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                       Repellat fugiat, laboriosam, voluptatem, optio vero odio
                       nam sit officia accusamus minus error nisi architecto
                       nulla ipsum dignissimos. Odit sed qui, dolorum!
                     </p>
-                    <Button color="deep-orange">
-                      <Fa icon="clone left" /> View project
-                    </Button>
+                    <MDBBtn color="deep-orange">
+                      <MDBIcon icon="clone left" /> View project
+                    </MDBBtn>
                   </div>
                 </div>
-              </Card>
-            </Col>
-          </Row>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
 
           <hr className="my-5" />
           <h2>Flipping card</h2>
 
-          <Row>
-            <Col style={{ minHeight: '26rem' }}>
-              <FlippingCard
+          <MDBRow>
+            <MDBCol style={{ minHeight: '26rem' }}>
+              <MDBRotatingCard
                 flipped={this.state.flipped}
                 className="text-center h-100 w-100"
                 style={colStyle}
               >
-                <Card className="face front">
-                  <CardUp>
+                <MDBCard className="face front">
+                  <MDBCardUp>
                     <img
                       className="card-img-top"
                       src="https://mdbootstrap.com/img/Photos/Others/photo7.jpg"
                       alt=""
                     />
-                  </CardUp>
-                  <Avatar className="mx-auto white" circle>
+                  </MDBCardUp>
+                  <MDBAvatar className="mx-auto white" circle>
                     <img
                       src="https://mdbootstrap.com/img/Photos/Avatars/img%20(20).jpg"
                       alt=""
                       className="rounded-circle"
                     />
-                  </Avatar>
-                  <CardBody>
+                  </MDBAvatar>
+                  <MDBCardBody>
                     <h4 className="font-weight-bold mb-3">Marie Johnson</h4>
                     <p className="font-weight-bold blue-text">Web developer</p>
                     <a
@@ -250,12 +235,12 @@ class FlippingCardPage extends React.Component {
                       data-card="card-1"
                       onClick={this.handleFlipping}
                     >
-                      <Fa icon="redo" /> Click here to rotate
+                      <MDBIcon icon="redo" /> Click here to rotate
                     </a>
-                  </CardBody>
-                </Card>
-                <Card className="face back">
-                  <CardBody>
+                  </MDBCardBody>
+                </MDBCard>
+                <MDBCard className="face back">
+                  <MDBCardBody>
                     <h4 className="font-weight-bold">About me</h4>
                     <hr />
                     <p>
@@ -268,22 +253,22 @@ class FlippingCardPage extends React.Component {
                     <ul className="list-inline py-2">
                       <li className="list-inline-item">
                         <a href="#!" className="p-2 fa-lg fb-ic">
-                          <Fa icon="facebook" brand />
+                          <MDBIcon icon="facebook" brand />
                         </a>
                       </li>
                       <li className="list-inline-item">
                         <a href="#!" className="p-2 fa-lg tw-ic">
-                          <Fa icon="twitter" brand />
+                          <MDBIcon icon="twitter" brand />
                         </a>
                       </li>
                       <li className="list-inline-item">
                         <a href="#!" className="p-2 fa-lg gplus-ic">
-                          <Fa icon="google-plus" brand />
+                          <MDBIcon icon="google-plus" brand />
                         </a>
                       </li>
                       <li className="list-inline-item">
                         <a href="#!" className="p-2 fa-lg li-ic">
-                          <Fa icon="linkedin" brand />
+                          <MDBIcon icon="linkedin" brand />
                         </a>
                       </li>
                     </ul>
@@ -293,69 +278,69 @@ class FlippingCardPage extends React.Component {
                       data-card="card-1"
                       onClick={this.handleFlipping}
                     >
-                      <Fa icon="undo" /> Click here to rotate back
+                      <MDBIcon icon="undo" /> Click here to rotate back
                     </a>
-                  </CardBody>
-                </Card>
-              </FlippingCard>
-            </Col>
-          </Row>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBRotatingCard>
+            </MDBCol>
+          </MDBRow>
 
           <hr className="my-5" />
           <h2>Action button and footer</h2>
 
-          <Row>
-            <Col style={colStyle}>
-              <Card cascade>
-                <CardImage
+          <MDBRow>
+            <MDBCol style={colStyle}>
+              <MDBCard cascade>
+                <MDBCardImage
                   cascade
                   className="img-fluid"
                   src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20%282%29.jpg"
                 />
-                <Button
+                <MDBBtn
                   floating
                   tag="a"
                   className="ml-auto mr-4 lighten-3 mdb-coalor"
                   action
                 >
-                  <Fa icon="chevron-right" />
-                </Button>
-                <CardBody cascade>
-                  <CardTitle>Card title</CardTitle>
-                  <CardText>
+                  <MDBIcon icon="chevron-right" />
+                </MDBBtn>
+                <MDBCardBody cascade>
+                  <MDBCardTitle>MDBCard title</MDBCardTitle>
+                  <MDBCardText>
                     Some quick example text to build on the card title and make
                     up the bulk of the card's content.
-                  </CardText>
-                </CardBody>
+                  </MDBCardText>
+                </MDBCardBody>
                 <div className="rounded-bottom mdb-color lighten-3 text-center pt-3">
                   <ul className="list-unstyled list-inline font-small">
                     <li className="list-inline-item pr-2 white-text">
-                      <Fa icon="clock" /> 05/10/2015
+                      <MDBIcon icon="clock" /> 05/10/2015
                     </li>
                     <li className="list-inline-item pr-2">
                       <a href="#!" className="white-text">
-                        <Fa icon="comments" /> 12
+                        <MDBIcon icon="comments" /> 12
                       </a>
                     </li>
                     <li className="list-inline-item pr-2">
                       <a href="#!" className="white-text">
-                        <Fa icon="facebook" brand /> 21
+                        <MDBIcon icon="facebook" brand /> 21
                       </a>
                     </li>
                     <li className="list-inline-item">
                       <a href="#!" className="white-text">
-                        <Fa icon="twitter" brand /> 5
+                        <MDBIcon icon="twitter" brand /> 5
                       </a>
                     </li>
                   </ul>
                 </div>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      </Container>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </MDBContainer>
     );
   }
 }
 
-export default FlippingCardPage;
+export default CardPage;
