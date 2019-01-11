626b6e0da3dc8038fd4e85ffa51673bf70ca7004
Anna Morawska <a.morawska@mdbootstrap.com>
2019-01-11 13:11:01 +0100

    Refractor ModalFormPage

--- docs/pages/pro/ModalFormPage.js
+++ docs/pages/pro/ModalFormPage.js
@@ -1,43 +1,24 @@
-import React from "react";
-import {
-  Container,
-  Button,
-  Modal,
-  ModalBody,
-  ModalHeader,
-  ModalFooter,
-  Row,
-  Input,
-  Fa,
-  Col,
-  TabContent,
-  TabPane,
-  Nav,
-  NavItem,
-  NavLink
-} from "mdbreact";
+import React, { Component } from "react";
+import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, ModalHeader, MDBModalFooter, MDBRow, MDBInput,  MDBIcon, MDBCol, MDBTabContent, MDBTabPane, MDBNav, MDBNavItem, MDBNavLink } from "mdbreact";
 import "./ModalFormPage.css";
-import DocsLink from "../DocsLink";
+import DocsLink from "./../../components/docsLink";
 
-class ModalPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      modal1: false,
-      modal2: false,
-      modal3: false,
-      activeItem: "1"
-    };
+class ModalFormPage extends Component {
+  state = {
+    modal1: false,
+    modal2: false,
+    modal3: false,
+    activeItem: "1"
   }
 
-  toggle(nr) {
+  toggle = nr => () => {
     let modalNumber = "modal" + nr;
     this.setState({
       [modalNumber]: !this.state[modalNumber]
     });
   }
 
-  toggleTab(tab) {
+  toggleTab = tab => () => {
     if (this.state.activeItem !== tab) {
       this.setState({
         activeItem: tab
@@ -47,56 +28,52 @@ class ModalPage extends React.Component {
 
   render() {
     return (
-      <Container>
+      <MDBContainer>
         <DocsLink
           title="Modal Form"
           href="https://mdbootstrap.com/docs/react/modals/basic/"
         />
         <h4 className="mt-4">Cascading modal register / login</h4>
-        <Row>
-          <Button rounded onClick={() => this.toggle(1)}>
+        <MDBRow>
+          <MDBBtn rounded onClick={this.toggle(1)}>
             Launch Modal Login/Register
-          </Button>
-          <Modal
+          </MDBBtn>
+          <MDBModal
             className="form-cascading"
             isOpen={this.state.modal1}
-            toggle={() => this.toggle(1)}
+            toggle={this.toggle(1)}
           >
-            <Nav
+            <MDBNav
               tabs
               className="md-tabs nav-justified tabs-2 light-blue darken-3"
               style={{ margin: "-1.5rem 1rem 0 1rem" }}
             >
-              <NavItem>
-                <NavLink
+              <MDBNavItem>
+                <MDBNavLink
                   className={this.state.activeItem === 1 ? "active" : ""}
                   to="#"
-                  onClick={() => {
-                    this.toggleTab("1");
-                  }}
+                  onClick={this.toggleTab("1")}
                 >
-                  <Fa icon="user" className="mr-1" />
+                  <MDBIcon icon="user" className="mr-1" />
                   Login
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
+                </MDBNavLink>
+              </MDBNavItem>
+              <MDBNavItem>
+                <MDBNavLink
                   className={this.state.activeItem === 2 ? "active" : ""}
                   to="#"
-                  onClick={() => {
-                    this.toggleTab("2");
-                  }}
+                  onClick={this.toggleTab("2")}
                 >
-                  <Fa icon="user-plus" className="mr-1" />
+                  <MDBIcon icon="user-plus" className="mr-1" />
                   Register
-                </NavLink>
-              </NavItem>
-            </Nav>
-            <TabContent activeItem={this.state.activeItem}>
-              <TabPane tabId="1">
-                <ModalBody className="mx-3">
+                </MDBNavLink>
+              </MDBNavItem>
+            </MDBNav>
+            <MDBTabContent activeItem={this.state.activeItem}>
+              <MDBTabPane tabId="1">
+                <MDBModalBody className="mx-3">
                   <form className=" mx-3 grey-text">
-                    <Input
+                    <MDBInput
                       label="Your email"
                       icon="envelope"
                       group
@@ -105,7 +82,7 @@ class ModalPage extends React.Component {
                       error="wrong"
                       success="right"
                     />
-                    <Input
+                    <MDBInput
                       label="Your password"
                       icon="lock"
                       group
@@ -113,27 +90,25 @@ class ModalPage extends React.Component {
                       validate
                     />
                   </form>
-                </ModalBody>
-                <ModalFooter className="justify-content-center mx-3">
-                  <Button
+                </MDBModalBody>
+                <MDBModalFooter className="justify-content-center mx-3">
+                  <MDBBtn
                     className="mb-4"
                     color="info"
-                    onClick={() => this.toggle(3)}
+                    onClick={this.toggle(3)}
                   >
-                    LOG IN <Fa icon="sign-in-alt" className="ml-1" />
-                  </Button>
-                  <Row
+                    LOG IN <MDBIcon icon="sign-in-alt" className="ml-1" />
+                  </MDBBtn>
+                  <MDBRow
                     className="w-100 justify-content-start pt-4"
                     style={{ borderTop: "1px solid #e9ecef" }}
                   >
                     <div id="options">
                       <p className="font-small grey-text">
-                        Not a member?{" "}
+                        N
                         <span
                           className="blue-text ml-1"
-                          onClick={() => {
-                            this.toggleTab("2");
-                          }}
+                          onClick={this.toggleTab("2")}
                         >
                           Sign Up
                         </span>
@@ -142,16 +117,16 @@ class ModalPage extends React.Component {
                         Forgot <span className="blue-text ml-1">password?</span>
                       </p>
                     </div>
-                    <Button outline color="info" onClick={() => this.toggle(1)}>
+                    <MDBBtn outline color="info" onClick={this.toggle(1)}>
                       CLOSE
-                    </Button>
-                  </Row>
-                </ModalFooter>
-              </TabPane>
-              <TabPane tabId="2">
-                <ModalBody className="mx-3">
+                    </MDBBtn>
+                  </MDBRow>
+                </MDBModalFooter>
+              </MDBTabPane>
+              <MDBTabPane tabId="2">
+                <MDBModalBody className="mx-3">
                   <form className=" mx-3 grey-text">
-                    <Input
+                    <MDBInput
                       label="Your email"
                       icon="envelope"
                       group
@@ -160,14 +135,14 @@ class ModalPage extends React.Component {
                       error="wrong"
                       success="right"
                     />
-                    <Input
+                    <MDBInput
                       label="Your password"
                       icon="lock"
                       group
                       type="password"
                       validate
                     />
-                    <Input
+                    <MDBInput
                       label="Repeat password"
                       icon="lock"
                       group
@@ -175,17 +150,17 @@ class ModalPage extends React.Component {
                       validate
                     />
                   </form>
-                </ModalBody>
-                <ModalFooter className="justify-content-center mx-3">
-                  <Button
+                </MDBModalBody>
+                <MDBModalFooter className="justify-content-center mx-3">
+                  <MDBBtn
                     className="mb-4"
                     color="info"
-                    onClick={() => this.toggle(3)}
+                    onClick={this.toggle(3)}
                   >
                     SIGN UP
-                    <Fa icon="sign-in-alt" className="ml-1" />
-                  </Button>
-                  <Row
+                    <MDBIcon icon="sign-in-alt" className="ml-1" />
+                  </MDBBtn>
+                  <MDBRow
                     className="w-100 justify-content-start pt-4"
                     style={{ borderTop: "1px solid #e9ecef" }}
                   >
@@ -194,107 +169,105 @@ class ModalPage extends React.Component {
                         Already have an account?
                         <span
                           className="blue-text ml-1"
-                          onClick={() => {
-                            this.toggleTab("1");
-                          }}
+                          onClick={this.toggleTab("1")}
                         >
                           Log in
                         </span>
                       </p>
                     </div>
-                    <Button outline color="info" onClick={() => this.toggle(1)}>
+                    <MDBBtn outline color="info" onClick={this.toggle(1)}>
                       CLOSE
-                    </Button>
-                  </Row>
-                </ModalFooter>
-              </TabPane>
-            </TabContent>
-          </Modal>
-        </Row>
+                    </MDBBtn>
+                  </MDBRow>
+                </MDBModalFooter>
+              </MDBTabPane>
+            </MDBTabContent>
+          </MDBModal>
+        </MDBRow>
 
         <h4 className="mt-4">Elegant modal login</h4>
-        <Row>
-          <Button rounded onClick={() => this.toggle(2)}>
+        <MDBRow>
+          <MDBBtn rounded onClick={this.toggle(2)}>
             Launch Modal Login Form
-          </Button>
-          <Modal
+          </MDBBtn>
+          <MDBModal
             className="form-elegant"
             isOpen={this.state.modal2}
-            toggle={() => this.toggle(2)}
+            toggle={this.toggle(2)}
           >
             <ModalHeader
               className="text-center"
               titleClass="w-100 font-weight-bold my-3"
-              toggle={() => this.toggle(2)}
+              toggle={this.toggle(2)}
             >
               Sign in
             </ModalHeader>
-            <ModalBody className="mx-3">
+            <MDBModalBody className="mx-3">
               <form className=" mx-3 grey-text">
-                <Input label="Your email" group type="email" validate />
-                <Input label="Your password" group type="password" validate />
+                <MDBInput label="Your email" group type="email" validate />
+                <MDBInput label="Your password" group type="password" validate />
                 <p className="font-small blue-text text-right">
                   Forgot password?
                 </p>
               </form>
-            </ModalBody>
-            <ModalFooter className="justify-content-center mx-4">
-              <Button
+            </MDBModalBody>
+            <MDBModalFooter className="justify-content-center mx-4">
+              <MDBBtn
                 block
                 rounded
                 gradient="blue"
-                onClick={() => this.toggle(2)}
+                onClick={this.toggle(2)}
               >
                 SIGN IN
-              </Button>
+              </MDBBtn>
               <p className="font-small dark-grey-text my-4">or Sign in with:</p>
-              <Row className="py-3">
-                <Button rounded color="white" className="mr-md-3">
-                  <Fa fab icon="facebook" className="text-center blue-text" />
-                </Button>
-                <Button rounded color="white" className="mr-md-3">
-                  <Fa fab icon="twitter" className="text-center blue-text" />
-                </Button>
-                <Button rounded color="white" className="mr-md-3">
-                  <Fa fab icon="google-plus" className="text-center blue-text" />
-                </Button>
-              </Row>
-              <Row
+              <MDBRow className="py-3">
+                <MDBBtn rounded color="white" className="mr-md-3">
+                  <MDBIcon fab icon="facebook" className="text-center blue-text" />
+                </MDBBtn>
+                <MDBBtn rounded color="white" className="mr-md-3">
+                  <MDBIcon fab icon="twitter" className="text-center blue-text" />
+                </MDBBtn>
+                <MDBBtn rounded color="white" className="mr-md-3">
+                  <MDBIcon fab icon="google-plus" className="text-center blue-text" />
+                </MDBBtn>
+              </MDBRow>
+              <MDBRow
                 className="mx-5 w-100 py-3 justify-content-end"
                 style={{ borderTop: "1px solid #e9ecef" }}
               >
                 <p className="font-small grey-text">
                   Not a member? <span className="blue-text ml-1"> Sign Up</span>
                 </p>
-              </Row>
-            </ModalFooter>
-          </Modal>
-        </Row>
+              </MDBRow>
+            </MDBModalFooter>
+          </MDBModal>
+        </MDBRow>
 
         <h4 className="mt-4">Dark modal register</h4>
-        <Row>
-          <Button rounded onClick={() => this.toggle(3)}>
+        <MDBRow>
+          <MDBBtn rounded onClick={this.toggle(3)}>
             Launch Modal Register Form
-          </Button>
-          <Modal
+          </MDBBtn>
+          <MDBModal
             className="form-dark"
             contentClassName="card card-image"
             isOpen={this.state.modal3}
-            toggle={() => this.toggle(3)}
+            toggle={this.toggle(3)}
           >
             <div className="text-white rgba-stylish-strong py-5 px-5 z-depth-4">
               <ModalHeader
                 className="text-center"
                 titleClass="w-100 font-weight-bold white-text"
-                toggle={() => this.toggle(3)}
+                toggle={this.toggle(3)}
               >
                 SIGN
                 <span className="green-text"> UP</span>
               </ModalHeader>
-              <ModalBody>
+              <MDBModalBody>
                 <form className="mx-3 grey-text">
-                  <Input label="Your email" group type="email" validate />
-                  <Input label="Your password" group type="password" validate />
+                  <MDBInput label="Your email" group type="email" validate />
+                  <MDBInput label="Your password" group type="password" validate />
                   <input
                     className="form-check-input"
                     type="checkbox"
@@ -306,36 +279,35 @@ class ModalPage extends React.Component {
                   >
                     Accept the
                     <span className="green-text font-weight-bold">
-                      {" "}
                       Terms and Conditions
                     </span>
                   </label>
                 </form>
-              </ModalBody>
-              <ModalFooter className="justify-content-center">
-                <Col md="12" className="mb-3">
-                  <Button
+              </MDBModalBody>
+              <MDBModalFooter className="justify-content-center">
+                <MDBCol md="12" className="mb-3">
+                  <MDBBtn
                     block
                     rounded
                     color="success"
-                    onClick={() => this.toggle(3)}
+                    onClick={this.toggle(3)}
                   >
                     SIGN UP
-                  </Button>
-                </Col>
-                <Col md="12" className="mb-3 text-right font-small">
+                  </MDBBtn>
+                </MDBCol>
+                <MDBCol md="12" className="mb-3 text-right font-small">
                   Have an account?
                   <span className="green-text ml-1 font-weight-bold">
                     Log in
                   </span>
-                </Col>
-              </ModalFooter>
+                </MDBCol>
+              </MDBModalFooter>
             </div>
-          </Modal>
-        </Row>
-      </Container>
+          </MDBModal>
+        </MDBRow>
+      </MDBContainer>
     );
   }
 }
 
-export default ModalPage;
+export default ModalFormPage;
