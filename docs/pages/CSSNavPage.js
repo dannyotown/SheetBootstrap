b1e476938ad4a450a159d7eeedfed5b5cc8dadc7
Anna Morawska <a.morawska@mdbootstrap.com>
2019-01-11 08:44:21 +0100

    Refractor CSSNavPage

--- docs/pages/CSSNavPage.js
+++ docs/pages/CSSNavPage.js
@@ -1,85 +1,29 @@
 import React from "react";
-import { Container, Row, Col, Jumbotron, Fa, NavLink } from "mdbreact";
+import { MDBContainer, MDBRow, MDBCol, MDBJumbotron, MDBIcon } from "mdbreact";
+import MenuLink from "./components/menuLink";
 
-class CSSPage extends React.Component {
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col md="8" className="mx-auto">
-            <Jumbotron className="mt-3">
-              <h1>
-                <Fa icon="css3" brand className="grey-text" /> CSS
-              </h1>
-              <ul className="list-unstyled example-components-list">
-                <h6 className="mt-3 grey-text">FREE </h6>
-                <NavLink
-                  className="list-group-item list-group-item-action"
-                  to="/css/animations"
-                >
-                  <h5
-                    style={{ margin: "0" }}
-                    className="justify-content-between d-flex align-items-center"
-                  >
-                    Animation
-                    <Fa icon="angle-right" />
-                  </h5>
-                </NavLink>
-                <NavLink
-                  className="list-group-item list-group-item-action"
-                  to="/css/hover"
-                >
-                  <h5
-                    style={{ margin: "0" }}
-                    className="justify-content-between d-flex align-items-center"
-                  >
-                    Hover effects
-                    <Fa icon="angle-right" />
-                  </h5>
-                </NavLink>
-                <NavLink
-                  className="list-group-item list-group-item-action"
-                  to="/css/icons"
-                >
-                  <h5
-                    style={{ margin: "0" }}
-                    className="justify-content-between d-flex align-items-center"
-                  >
-                    Icons
-                    <Fa icon="angle-right" />
-                  </h5>
-                </NavLink>
-                <NavLink
-                  className="list-group-item list-group-item-action"
-                  to="/css/jumbotron"
-                >
-                  <h5
-                    style={{ margin: "0" }}
-                    className="justify-content-between d-flex align-items-center"
-                  >
-                    Jumbotron
-                    <Fa icon="angle-right" />
-                  </h5>
-                </NavLink>
-                <NavLink
-                  className="list-group-item list-group-item-action"
-                  to="/css/masks"
-                >
-                  <h5
-                    style={{ margin: "0" }}
-                    className="justify-content-between d-flex align-items-center"
-                  >
-                    Masks
-                    <Fa icon="angle-right" />
-                  </h5>
-                </NavLink>
-              </ul>
-            </Jumbotron>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+const CSSPage = () => {
+  return (
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol md="8" className="mx-auto">
+          <MDBJumbotron className="mt-3">
+            <h1>
+              <MDBIcon icon="css3" brand className="grey-text" /> CSS
+            </h1>
+            <ul className="list-unstyled example-components-list">
+              <h6 className="mt-3 grey-text">FREE </h6>
+              <MenuLink to="/css/animations" title="Animation" />
+              <MenuLink to="/css/hover" title="Hover effects" />
+              <MenuLink to="/css/icons" title="Icons" />
+              <MenuLink to="/css/jumbotron" title="MDBJumbotron" />
+              <MenuLink to="/css/masks" title="Masks"  />
+            </ul>
+          </MDBJumbotron>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
 }
 
 export default CSSPage;
