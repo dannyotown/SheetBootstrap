46a09a19b646c421b0c68695465b8ffd61e0de10
Anna Morawska <a.morawska@mdbootstrap.com>
2019-01-15 13:17:35 +0100

    Turn iFramePage component to stateless component

--- docs/pages/IframePage.js
+++ docs/pages/IframePage.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import { MDBContainer, MDBIframe } from "mdbreact";
 import DocsLink from "./../components/docsLink";
 
-class IframePage extends Component {
-  render() {
-    return (
-      <MDBContainer className="text-center">
-        <DocsLink
-          title="Iframe"
-          href="https://mdbootstrap.com/plugins/react/iframe/"
-        />
-        <MDBIframe src="https://www.youtube.com/embed/v64KOxKVLVg" />
-      </MDBContainer>
-    );
-  }
+const IframePage = () => {
+  return (
+    <MDBContainer className="text-center">
+      <DocsLink
+        title="Iframe"
+        href="https://mdbootstrap.com/plugins/react/iframe/"
+      />
+      <MDBIframe src="https://www.youtube.com/embed/v64KOxKVLVg" />
+    </MDBContainer>
+  );
 }
 
 export default IframePage;
