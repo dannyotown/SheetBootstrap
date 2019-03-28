373852dfd812917d4b5fb9cb4a502352e0c0b146
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-03-28 13:25:00 +0100

    Update all elements usign Tooltip or Popover
    ModalPage, eCommercePage, EcommercePage, Step

--- src/components/pro/Step.js
+++ src/components/pro/Step.js
@@ -1,27 +1,10 @@
 import React from "react";
 import classNames from "classnames";
 import Tooltip from "../Popper";
+import Button from "../Button";
 import Waves from "../Waves";
 
 class Step extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cursorPos: {}
-    };
-  }
-
-  handleClick(e) {
-    // Get Cursor Position
-    e.preventDefault();
-    let cursorPos = {
-      top: e.clientY,
-      left: e.clientX,
-      time: Date.now()
-    };
-    this.setState({ cursorPos: cursorPos });
-  }
-
   render() {
     const { tag: Tag, form, icon, stepName, vertical } = this.props;
 
@@ -46,38 +29,28 @@ class Step extends React.Component {
         <Tag className={stepClass} onClick={this.props.onClick}>
           <Tooltip
             placement="top"
-            componentClass="btn btn-circle-2 btn-blue-grey waves-effect"
-            tag="a"
-            type="button"
-            component="div"
-            tooltipContent={stepName}
           >
-            <i
-              className={iconClass}
-              onTouchStart={this.handleClick.bind(this)}
-              onMouseDown={this.handleClick.bind(this)}
-            />
-            <Waves cursorPos={this.state.cursorPos} dark />
+            <Button
+              className="btn-circle-2 btn-blue-grey"
+            >
+              <i className={iconClass} />
+            </Button>
+            <div>{stepName}</div>
           </Tooltip>
-        </Tag>
+        </Tag >
       );
     } else if (icon && vertical) {
       step = (
         <Tag className={stepClass} onClick={this.props.onClick}>
           <Tooltip
             placement="top"
-            componentClass="btn btn-circle-3 btn-blue-grey waves-effect"
-            tag="a"
-            type="button"
-            component="div"
-            tooltipContent={stepName}
           >
-            <i
-              className={iconClass}
-              onTouchStart={this.handleClick.bind(this)}
-              onMouseDown={this.handleClick.bind(this)}
-            />
-            <Waves cursorPos={this.state.cursorPos} dark />
+            <Button
+              className="btn-circle-3 btn-blue-grey"
+            >
+              <i className={iconClass} />
+            </Button>
+            <div>{stepName}</div>
           </Tooltip>
         </Tag>
       );
