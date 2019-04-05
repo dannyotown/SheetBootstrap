38e6884a2213ea257528adc06793b18ecbf54d1a
Jakub Mandra <j.mandra@mdbootstrap.com>
2019-04-05 14:07:01 +0200

    Move react to peerDeps, upgrade rollup config, import addons in sass

--- rollup.config.js
+++ rollup.config.js
@@ -4,20 +4,18 @@ import postcss from 'rollup-plugin-postcss';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 import commonjs from 'rollup-plugin-commonjs';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 const dependencies = Object.keys(require("./package.json").dependencies)
 
 const plugins = [
+  peerDepsExternal(),
   replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
   nodeResolve({
     jsnext: true,
     main: true
   }),
   commonjs({
-    include: 'node_modules/**',
-    namedExports: {
-      'node_modules/react/index.js': ['Component', 'PureComponent', 'Fragment', 'Children', 'createElement', 'createContext', 'forwardRef', 'useState', 'useEffect'],
-      'node_modules/react-dom/index.js': ['render', 'findDOMNode']
-    }
+    include: 'node_modules/**'
   }),
   babel({
     exclude: 'node_modules/**'
