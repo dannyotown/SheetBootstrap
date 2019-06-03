4765b2a5492f00f4db43aa31ee5d8cba3c5bea55
Aliaksandr <a.andrasiuk@mdbootstrap.com>
2019-06-03 10:55:24 +0200

    Add tabs with border example in TabsPage component

--- docs/pages/pro/TabsPage.js
+++ docs/pages/pro/TabsPage.js
@@ -188,35 +188,34 @@ class TabsPage extends Component {
               </MDBTabContent>
 
               <h2 className="mt-5">With dropdown</h2>
-              <MDBContainer>
-                <MDBNav tabs>
-                  <MDBNavItem>
-                    <MDBNavLink active to="#!">
-                      Active
-                    </MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBDropdown>
-                      <MDBDropdownToggle nav caret color="default">
-                        Dropdown
-                      </MDBDropdownToggle>
-                      <MDBDropdownMenu color="default">
-                        <MDBDropdownItem>Action</MDBDropdownItem>
-                        <MDBDropdownItem>Another Action</MDBDropdownItem>
-                        <MDBDropdownItem>Something else here</MDBDropdownItem>
-                        <MDBDropdownItem divider />
-                        <MDBDropdownItem>Separated link</MDBDropdownItem>
-                      </MDBDropdownMenu>
-                    </MDBDropdown>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="#!">Link</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem className="disabled">
-                    <MDBNavLink to="#!">Disabled</MDBNavLink>
-                  </MDBNavItem>
-                </MDBNav>
-              </MDBContainer>
+
+              <MDBNav tabs>
+                <MDBNavItem>
+                  <MDBNavLink active to="#!">
+                    Active
+                  </MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBDropdown>
+                    <MDBDropdownToggle nav caret color="default">
+                      Dropdown
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu color="default">
+                      <MDBDropdownItem>Action</MDBDropdownItem>
+                      <MDBDropdownItem>Another Action</MDBDropdownItem>
+                      <MDBDropdownItem>Something else here</MDBDropdownItem>
+                      <MDBDropdownItem divider />
+                      <MDBDropdownItem>Separated link</MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBNavLink to="#!">Link</MDBNavLink>
+                </MDBNavItem>
+                <MDBNavItem className="disabled">
+                  <MDBNavLink to="#!">Disabled</MDBNavLink>
+                </MDBNavItem>
+              </MDBNav>
 
               <h2 className="mt-5">Tabs justified</h2>
               <MDBNav tabs className="nav-justified" color="indigo">
@@ -299,7 +298,7 @@ class TabsPage extends Component {
                 </MDBTabPane>
               </MDBTabContent>
 
-              <h2 className="mt-5">Classic tabs</h2>
+              <h2 className="mt-5">Classic tabs with shadow</h2>
               <div className="classic-tabs">
                 <MDBNav classicTabs color="cyan">
                   <MDBNavItem>
@@ -397,104 +396,208 @@ class TabsPage extends Component {
                 </MDBTabContent>
               </div>
 
+              <h2 className="mt-5">Classic tabs with border</h2>
               <div className="classic-tabs">
-              <MDBNav classicTabs color="orange" className="mt-5">
-          <MDBNavItem>
-            <MDBNavLink
-              to="#"
-              active={this.state.activeItemClassicTabs3 === "1"}
-              onClick={this.toggleClassicTabs3("1")}
-            >
-              <MDBIcon icon="user" size="2x" />
-              <br />
-              Profile
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink
-              to="#"
-              active={this.state.activeItemClassicTabs3 === "2"}
-              onClick={this.toggleClassicTabs3("2")}
-            >
-              <MDBIcon icon="heart" size="2x" />
-              <br />
-              Follow
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink
-              to="#"
-              active={this.state.activeItemClassicTabs3 === "3"}
-              onClick={this.toggleClassicTabs3("3")}
-            >
-              <MDBIcon icon="envelope" size="2x" />
-              <br />
-              Contact
-            </MDBNavLink>
-          </MDBNavItem>
-          <MDBNavItem>
-            <MDBNavLink
-              to="#"
-              active={this.state.activeItemClassicTabs3 === "4"}
-              onClick={this.toggleClassicTabs3("4")}
-            >
-              <MDBIcon icon="star" size="2x" />
-              <br />
-              Be Awesome
-            </MDBNavLink>
-          </MDBNavItem>
-        </MDBNav>
-        <MDBTabContent
-          className="card mb-5"
-          activeItem={this.state.activeItemClassicTabs3}
-        >
-          <MDBTabPane tabId="1">
-            <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-              velit, sed quia non numquam eius modi tempora incidunt ut labore
-              et dolore magnam aliquam quaerat voluptatem.
-            </p>
-          </MDBTabPane>
-          <MDBTabPane tabId="2">
-            <p>
-              Ut enim ad minima veniam, quis nostrum exercitationem ullam
-              corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-              consequatur? Quis autem vel eum iure reprehenderit qui in ea
-              voluptate velit esse quam nihil molestiae consequatur, vel illum
-              qui dolorem eum fugiat quo voluptas nulla pariatur?
-            </p>
-          </MDBTabPane>
-          <MDBTabPane tabId="3">
-            <p>
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui
-              blanditiis praesentium voluptatum deleniti atque corrupti quos
-              dolores et quas molestias excepturi sint occaecati cupiditate non
-              provident, similique sunt in culpa qui officia deserunt mollitia
-              animi, id est laborum et dolorum fuga. Et harum quidem rerum
-              facilis est et expedita distinctio. Nam libero tempore, cum soluta
-              nobis est eligendi optio cumque nihil impedit quo minus id quod
-              maxime placeat facere possimus, omnis voluptas assumenda est,
-              omnis dolor repellendus.
-            </p>
-          </MDBTabPane>
-          <MDBTabPane tabId="4">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </MDBTabPane>
-        </MDBTabContent>
+                <MDBNav classicTabs color="secondary">
+                  <MDBNavItem>
+                    <MDBNavLink
+                      to="#"
+                      active={this.state.activeItemClassicTabs2 === "1"}
+                      onClick={this.toggleClassicTabs2("1")}
+                    >
+                      Profile
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink
+                      to="#"
+                      active={this.state.activeItemClassicTabs2 === "2"}
+                      onClick={this.toggleClassicTabs2("2")}
+                    >
+                      Follow
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink
+                      to="#"
+                      active={this.state.activeItemClassicTabs2 === "3"}
+                      onClick={this.toggleClassicTabs2("3")}
+                    >
+                      Contact
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink
+                      to="#"
+                      active={this.state.activeItemClassicTabs2 === "4"}
+                      onClick={this.toggleClassicTabs2("4")}
+                    >
+                      Be Awesome
+                    </MDBNavLink>
+                  </MDBNavItem>
+                </MDBNav>
+                <MDBTabContent
+                  className="border-right border-bottom border-left rounded-bottom bg-white"
+                  activeItem={this.state.activeItemClassicTabs2}
+                >
+                  <MDBTabPane tabId="1">
+                    <p>
+                      Sed ut perspiciatis unde omnis iste natus error sit
+                      voluptatem accusantium doloremque laudantium, totam rem
+                      aperiam, eaque ipsa quae ab illo inventore veritatis et
+                      quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                      enim ipsam voluptatem quia voluptas sit aspernatur aut
+                      odit aut fugit, sed quia consequuntur magni dolores eos
+                      qui ratione voluptatem sequi nesciunt. Neque porro
+                      quisquam est, qui dolorem ipsum quia dolor sit amet,
+                      consectetur, adipisci velit, sed quia non numquam eius
+                      modi tempora incidunt ut labore et dolore magnam aliquam
+                      quaerat voluptatem.
+                    </p>
+                  </MDBTabPane>
+                  <MDBTabPane tabId="2">
+                    <p>
+                      Ut enim ad minima veniam, quis nostrum exercitationem
+                      ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+                      commodi consequatur? Quis autem vel eum iure reprehenderit
+                      qui in ea voluptate velit esse quam nihil molestiae
+                      consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                      nulla pariatur?
+                    </p>
+                  </MDBTabPane>
+                  <MDBTabPane tabId="3">
+                    <p>
+                      At vero eos et accusamus et iusto odio dignissimos ducimus
+                      qui blanditiis praesentium voluptatum deleniti atque
+                      corrupti quos dolores et quas molestias excepturi sint
+                      occaecati cupiditate non provident, similique sunt in
+                      culpa qui officia deserunt mollitia animi, id est laborum
+                      et dolorum fuga. Et harum quidem rerum facilis est et
+                      expedita distinctio. Nam libero tempore, cum soluta nobis
+                      est eligendi optio cumque nihil impedit quo minus id quod
+                      maxime placeat facere possimus, omnis voluptas assumenda
+                      est, omnis dolor repellendus.
+                    </p>
+                  </MDBTabPane>
+                  <MDBTabPane tabId="4">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </MDBTabPane>
+                </MDBTabContent>
+              </div>
+
+              <h2 className="mt-5">Classic tabs with icons</h2>
+              <div className="classic-tabs">
+                <MDBNav classicTabs color="orange">
+                  <MDBNavItem>
+                    <MDBNavLink
+                      to="#"
+                      active={this.state.activeItemClassicTabs3 === "1"}
+                      onClick={this.toggleClassicTabs3("1")}
+                    >
+                      <MDBIcon icon="user" size="2x" />
+                      <br />
+                      Profile
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink
+                      to="#"
+                      active={this.state.activeItemClassicTabs3 === "2"}
+                      onClick={this.toggleClassicTabs3("2")}
+                    >
+                      <MDBIcon icon="heart" size="2x" />
+                      <br />
+                      Follow
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink
+                      to="#"
+                      active={this.state.activeItemClassicTabs3 === "3"}
+                      onClick={this.toggleClassicTabs3("3")}
+                    >
+                      <MDBIcon icon="envelope" size="2x" />
+                      <br />
+                      Contact
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink
+                      to="#"
+                      active={this.state.activeItemClassicTabs3 === "4"}
+                      onClick={this.toggleClassicTabs3("4")}
+                    >
+                      <MDBIcon icon="star" size="2x" />
+                      <br />
+                      Be Awesome
+                    </MDBNavLink>
+                  </MDBNavItem>
+                </MDBNav>
+                <MDBTabContent
+                  className="card mb-5"
+                  activeItem={this.state.activeItemClassicTabs3}
+                >
+                  <MDBTabPane tabId="1">
+                    <p>
+                      Sed ut perspiciatis unde omnis iste natus error sit
+                      voluptatem accusantium doloremque laudantium, totam rem
+                      aperiam, eaque ipsa quae ab illo inventore veritatis et
+                      quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                      enim ipsam voluptatem quia voluptas sit aspernatur aut
+                      odit aut fugit, sed quia consequuntur magni dolores eos
+                      qui ratione voluptatem sequi nesciunt. Neque porro
+                      quisquam est, qui dolorem ipsum quia dolor sit amet,
+                      consectetur, adipisci velit, sed quia non numquam eius
+                      modi tempora incidunt ut labore et dolore magnam aliquam
+                      quaerat voluptatem.
+                    </p>
+                  </MDBTabPane>
+                  <MDBTabPane tabId="2">
+                    <p>
+                      Ut enim ad minima veniam, quis nostrum exercitationem
+                      ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
+                      commodi consequatur? Quis autem vel eum iure reprehenderit
+                      qui in ea voluptate velit esse quam nihil molestiae
+                      consequatur, vel illum qui dolorem eum fugiat quo voluptas
+                      nulla pariatur?
+                    </p>
+                  </MDBTabPane>
+                  <MDBTabPane tabId="3">
+                    <p>
+                      At vero eos et accusamus et iusto odio dignissimos ducimus
+                      qui blanditiis praesentium voluptatum deleniti atque
+                      corrupti quos dolores et quas molestias excepturi sint
+                      occaecati cupiditate non provident, similique sunt in
+                      culpa qui officia deserunt mollitia animi, id est laborum
+                      et dolorum fuga. Et harum quidem rerum facilis est et
+                      expedita distinctio. Nam libero tempore, cum soluta nobis
+                      est eligendi optio cumque nihil impedit quo minus id quod
+                      maxime placeat facere possimus, omnis voluptas assumenda
+                      est, omnis dolor repellendus.
+                    </p>
+                  </MDBTabPane>
+                  <MDBTabPane tabId="4">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit
+                      esse cillum dolore eu fugiat nulla pariatur. Excepteur
+                      sint occaecat cupidatat non proident, sunt in culpa qui
+                      officia deserunt mollit anim id est laborum.
+                    </p>
+                  </MDBTabPane>
+                </MDBTabContent>
               </div>
 
               <h2 className="mt-5">Pills within the tabs</h2>
