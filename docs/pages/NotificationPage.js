19490b020ddfbe61ef176eb11477649771a807d1
k.stepien <k.stepien@mdbootstrap.com>
2019-06-05 12:43:38 +0200

    add example with autohide and aria-data

--- docs/pages/NotificationPage.js
+++ docs/pages/NotificationPage.js
@@ -23,6 +23,7 @@ class NotificationPage extends Component {
         <MDBNotification
           show
           fade
+          role="alert"
           //autohide={3000} // by default = ∞ ms
         >
           <MDBNotification header>
@@ -50,7 +51,7 @@ class NotificationPage extends Component {
 
         <h2 className="mt-5">Translucent</h2>
         <MDBContainer className="grey darken-3 p-3">
-          <MDBNotification show fade>
+          <MDBNotification show fade role="alert">
             <MDBNotification header>
               <svg
                 className="rounded mr-2"
@@ -79,7 +80,7 @@ class NotificationPage extends Component {
         </MDBContainer>
 
         <h2 className="mt-5">Translucent</h2>
-        <MDBNotification show fade>
+        <MDBNotification show fade role="alert">
           <MDBNotification header>
             <svg
               className="rounded mr-2"
@@ -100,7 +101,7 @@ class NotificationPage extends Component {
           </MDBNotification>
           <MDBNotification body>See? Just like this.</MDBNotification>
         </MDBNotification>
-        <MDBNotification show fade>
+        <MDBNotification show fade role="alert">
           <MDBNotification header>
             <svg
               className="rounded mr-2"
@@ -130,6 +131,7 @@ class NotificationPage extends Component {
             <MDBNotification
               show
               fade
+              role="alert"
               style={{
                 position: "absolute",
                 top: 0,
@@ -172,7 +174,7 @@ class NotificationPage extends Component {
                 right: 0
               }}
             >
-              <MDBNotification show fade>
+              <MDBNotification show fade role="alert">
                 <MDBNotification header>
                   <svg
                     className="rounded mr-2"
@@ -196,7 +198,7 @@ class NotificationPage extends Component {
                 </MDBNotification>
                 <MDBNotification body>See? Just like this.</MDBNotification>
               </MDBNotification>
-              <MDBNotification show fade>
+              <MDBNotification show fade role="alert">
                 <MDBNotification header>
                   <svg
                     className="rounded mr-2"
@@ -227,6 +229,36 @@ class NotificationPage extends Component {
         </MDBContainer>
 
         <h2 className="mt-5">Accessibility</h2>
+        <MDBNotification
+          show
+          fade
+          role="alert"
+          aria-live="polite"
+          aria-atomic="true"
+          autohide={10000} // by default = ∞ ms
+        >
+          <MDBNotification header>
+            <svg
+              className="rounded mr-2"
+              width="20"
+              height="20"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="xMidYMid slice"
+              focusable="false"
+              role="img"
+            >
+              <rect fill="#007aff" width="100%" height="100%" />
+            </svg>
+            <strong className="mr-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+            <button className="ml-2 mb-1 close" onClick={this.hideNotification}>
+              &times;
+            </button>
+          </MDBNotification>
+          <MDBNotification body>
+            Hello, world! This is a toast message.
+          </MDBNotification>
+        </MDBNotification>
       </MDBContainer>
     );
   }
