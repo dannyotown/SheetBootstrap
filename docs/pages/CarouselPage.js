338d061a8fb7a78170ffeeb656904f22b0c250d2
k.stepien <k.stepien@mdbootstrap.com>
2019-06-21 13:02:32 +0200

    Add new props which stop slide when mouse hover

--- docs/pages/CarouselPage.js
+++ docs/pages/CarouselPage.js
@@ -203,6 +203,51 @@ const CarouselPage = () => {
           </MDBCarouselItem>
         </MDBCarouselInner>
       </MDBCarousel>
+
+
+      <h4 className="mt-5 mb-3">Stop when mouse is on the carousel</h4>
+      <MDBCarousel
+        interval={1500}
+        onHoverStop
+        activeItem={1}
+        length={3}
+        showControls={true}
+        showIndicators={true}
+        className="z-depth-1"
+      >
+        <MDBCarouselInner>
+          <MDBCarouselItem itemId="1">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
+                alt="First slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="2">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
+                alt="Second slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+          <MDBCarouselItem itemId="3">
+            <MDBView>
+              <img
+                className="d-block w-100"
+                src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+                alt="Third slide"
+              />
+            </MDBView>
+          </MDBCarouselItem>
+        </MDBCarouselInner>
+      </MDBCarousel>
+
+
+
     </MDBContainer>
   );
 }
