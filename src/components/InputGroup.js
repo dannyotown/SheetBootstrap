890bd22db58441b27222530279ef9563dd030ff9
k.stepien <k.stepien@mdbootstrap.com>
2019-06-25 13:11:44 +0200

    Change class component to function

--- src/components/InputGroup.js
+++ src/components/InputGroup.js
@@ -2,89 +2,86 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
-class InputGroup extends React.Component {
-  render() {
-    const {
-      append,
-      appendClassName,
-      ariaLabel,
-      className,
-      children,
-      inputClassName,
-      inputID,
-      inputName,
-      inputs,
-      inputType,
-      inputValue,
-      placeholder,
-      label,
-      material,
-      prepend,
-      prependClassName,
-      size,
-      textClassName,
-      tag: Tag,
-      inputTag: InputTag,
-      ...attributes
-    } = this.props;
+const InputGroup = ({
+  append,
+  appendClassName,
+  ariaLabel,
+  className,
+  children,
+  inputClassName,
+  inputID,
+  inputName,
+  inputs,
+  inputType,
+  inputValue,
+  placeholder,
+  label,
+  material,
+  prepend,
+  prependClassName,
+  size,
+  textClassName,
+  tag: Tag,
+  inputTag: InputTag,
+  ...attributes
+}) => {
+  const classes = classNames(
+    "input-group",
+    material && "md-form",
+    size && `input-group-${size}`,
+    className
+  );
 
-    const classes = classNames(
-      "input-group",
-      material && "md-form",
-      size && `input-group-${size}`,
-      className
-    );
+  const inputClassNames = classNames("form-control", inputClassName);
 
-    const inputClassNames = classNames("form-control", inputClassName);
+  const prependClassNames = classNames(
+    "input-group-prepend",
+    prependClassName
+  );
 
-    const prependClassNames = classNames(
-      "input-group-prepend",
-      prependClassName
-    );
+  const appendClassNames = classNames("input-group-append", appendClassName);
 
-    const appendClassNames = classNames("input-group-append", appendClassName);
+  const textClassNames = classNames(
+    "input-group-text",
+    material && "md-addon",
+    textClassName
+  );
 
-    const textClassNames = classNames(
-      "input-group-text",
-      material && "md-addon",
-      textClassName
-    );
+  return (
+    <Tag {...attributes} className={classes}>
+      {prepend && (
+        <div className={prependClassNames}>
+          {typeof prepend === "string" ? (
+            <span className={textClassNames}>{prepend}</span>
+          ) : (
+            prepend
+          )}
+        </div>
+      )}
+      {inputs || (
+        <InputTag
+          type={inputType}
+          className={inputClassNames}
+          id={inputID}
+          value={inputValue}
+          name={inputName}
+          placeholder={placeholder}
+          aria-label={ariaLabel}
+        />
+      )}
+      {append && (
+        <div className={appendClassNames}>
+          {typeof append === "string" ? (
+            <span className={textClassNames}>{append}</span>
+          ) : (
+            append
+          )}
+        </div>
+      )}
+      {children}
+    </Tag>
+  );
 
-    return (
-      <Tag {...attributes} className={classes}>
-        {prepend && (
-          <div className={prependClassNames}>
-            {typeof prepend === "string" ? (
-              <span className={textClassNames}>{prepend}</span>
-            ) : (
-              prepend
-            )}
-          </div>
-        )}
-        {inputs || (
-          <InputTag
-            type={inputType}
-            className={inputClassNames}
-            id={inputID}
-            value={inputValue}
-            name={inputName}
-            placeholder={placeholder}
-            aria-label={ariaLabel}
-          />
-        )}
-        {append && (
-          <div className={appendClassNames}>
-            {typeof append === "string" ? (
-              <span className={textClassNames}>{append}</span>
-            ) : (
-              append
-            )}
-          </div>
-        )}
-        {children}
-      </Tag>
-    );
-  }
 }
 
 InputGroup.propTypes = {
