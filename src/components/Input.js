cf411c208a09d3b8a2d37b9711bec712482fffa2
k.stepien <k.stepien@mdbootstrap.com>
2019-07-02 11:13:42 +0200

    Fix label if default value && value  === 0 and add number propTypes

--- src/components/Input.js
+++ src/components/Input.js
@@ -107,8 +107,8 @@ class Input extends React.Component {
       valueDefault,
       ...attributes
     } = this.props;
-
-    let isNotEmpty = !!this.state.innerValue || !!hint || this.state.isFocused;
+    
+    let isNotEmpty = !!this.state.innerValue || !!hint || this.state.isFocused || `${this.state.innerValue}`.length > 0 ;
     let Tag = "";
     let formControlClass = "";
 
@@ -244,7 +244,7 @@ Input.propTypes = {
   type: PropTypes.string,
   validate: PropTypes.bool,
   value: PropTypes.string,
-  valueDefault: PropTypes.string
+  valueDefault: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 };
 
 Input.defaultProps = {
