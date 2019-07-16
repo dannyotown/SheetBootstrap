6573200f742403f1edfe4efdb1a7449fab26125e
Aliaksandr <a.andrasiuk@mdbootstrap.com>
2019-07-16 13:10:26 +0200

    Update layout for HoverPage

--- docs/pages/HoverPage.js
+++ docs/pages/HoverPage.js
@@ -1,76 +1,85 @@
 import React from "react";
 import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 import DocsLink from "../components/docsLink";
+import SectionContainer from "../components/sectionContainer";
 
 const HoverPage = () => {
   return (
     <MDBContainer className="mt-5">
-      <DocsLink
-        title="Hover Effects"
-        href="https://mdbootstrap.com/docs/react/css/hover-effects/"
-      />
-      <MDBRow>
-        <MDBCol md="4">
-          <MDBView hover>
+      <DocsLink title="Hover Effects" href="https://mdbootstrap.com/docs/react/css/hover-effects/" />
+
+        <MDBCol md="8">
+          <SectionContainer header="Strong overlay">
+            <MDBView hover>
+              <img
+                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(75).jpg"
+                className="img-fluid"
+                alt=""
+              />
+              <MDBMask className="flex-center" overlay="red-strong">
+                <p className="white-text">Strong overlay</p>
+              </MDBMask>
+            </MDBView>
+          </SectionContainer>
+        </MDBCol>
+
+        <MDBCol md="8">
+          <SectionContainer header="Light overlay">
+            <MDBView hover>
+              <img
+                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(75).jpg"
+                className="img-fluid"
+                alt=""
+              />
+              <MDBMask className="flex-center" overlay="red-light">
+                <p className="white-text">Light overlay</p>
+              </MDBMask>
+            </MDBView>
+          </SectionContainer>
+        </MDBCol>
+
+        <MDBCol md="8">
+          <SectionContainer header="Super light overlay">
+            <MDBView hover>
+              <img
+                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(75).jpg"
+                className="img-fluid"
+                alt=""
+              />
+              <MDBMask className="flex-center" overlay="red-slight">
+                <p className="white-text">Super light overlay</p>
+              </MDBMask>
+            </MDBView>
+          </SectionContainer>
+        </MDBCol>
+
+        <MDBCol md="8">
+          <SectionContainer header="Zoom effect">
+            <MDBView hover zoom>
+              <img
+                src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(66).jpg"
+                className="img-fluid"
+                alt=""
+              />
+              <MDBMask className="flex-center">
+                <p className="white-text">Zoom effect</p>
+              </MDBMask>
+            </MDBView>
+          </SectionContainer>
+        </MDBCol>
+
+        <MDBCol md="8">
+          <SectionContainer header="Shadow effect">
             <img
-              src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(75).jpg"
-              className="img-fluid"
+              src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(42).jpg"
+              className="img-fluid hoverable"
               alt=""
             />
-            <MDBMask className="flex-center" overlay="red-strong">
-              <p className="white-text">Strong overlay</p>
-            </MDBMask>
-          </MDBView>
+          </SectionContainer>
         </MDBCol>
-        <MDBCol md="4">
-          <MDBView hover>
-            <img
-              src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(75).jpg"
-              className="img-fluid"
-              alt=""
-            />
-            <MDBMask className="flex-center" overlay="red-light">
-              <p className="white-text">Light overlay</p>
-            </MDBMask>
-          </MDBView>
-        </MDBCol>
-        <MDBCol md="4">
-          <MDBView hover>
-            <img
-              src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(75).jpg"
-              className="img-fluid"
-              alt=""
-            />
-            <MDBMask className="flex-center" overlay="red-slight">
-              <p className="white-text">Super light overlay</p>
-            </MDBMask>
-          </MDBView>
-        </MDBCol>
-      </MDBRow>
-      <MDBRow className="mt-4">
-        <MDBCol md="6">
-          <MDBView hover zoom>
-            <img
-              src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(66).jpg"
-              className="img-fluid"
-              alt=""
-            />
-            <MDBMask className="flex-center">
-              <p className="white-text">Zoom effect</p>
-            </MDBMask>
-          </MDBView>
-        </MDBCol>
-        <MDBCol md="6">
-          <h5 className="text-center">Shadow effect</h5>
-          <img
-            src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/6-col/img%20(42).jpg"
-            className="img-fluid rounded-circle hoverable"
-            alt=""
-          />
-        </MDBCol>
-      </MDBRow>
+      
     </MDBContainer>
   );
-}
+};
 
 export default HoverPage;
