ebd8b2487a1c74e05e74d56cb1ca0826523bb22d
k.stepien <k.stepien@mdbootstrap.com>
2019-07-17 09:58:41 +0200

    Change layout for NavPage's

--- docs/pages/pro/NavsPage.js
+++ docs/pages/pro/NavsPage.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
-import { MDBContainer, MDBNav, MDBNavItem, MDBNavLink, MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from "mdbreact";
+import {
+  MDBContainer,
+  MDBNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBRow,
+  MDBCol
+} from "mdbreact";
 import DocsLink from "../../components/docsLink";
+import SectionContainer from "../../components/sectionContainer";
 
 class NavsPage extends Component {
-
   render() {
     return (
       <MDBContainer>
@@ -12,138 +23,169 @@ class NavsPage extends Component {
           href="https://mdbootstrap.com/docs/react/navigation/navs/"
         />
 
-        <h2 className="mt-5">Basic MDB Navs with Tabs & Pills</h2>
-        <MDBContainer className="mt-4">
-          <MDBNav tabs color="indigo">
-            <MDBNavItem>
-              <MDBNavLink active to="#!">Active</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 1</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 2</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 3</MDBNavLink>
-            </MDBNavItem>
-          </MDBNav>
-        </MDBContainer>
+        <SectionContainer title="Basic MDB Navs with Tabs & Pills" noBorder>
+          <MDBRow>
+            <MDBCol md="6">
+              <SectionContainer className="pb-5">
+                <MDBNav tabs color="indigo">
+                  <MDBNavItem>
+                    <MDBNavLink active to="#!">
+                      Active
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 1</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 2</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 3</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNav>
+              </SectionContainer>
+            </MDBCol>
+            <MDBCol md="6">
+              <SectionContainer className="pb-4">
+                <MDBNav pills color="unique" className="mb-2">
+                  <MDBNavItem>
+                    <MDBNavLink active to="#!">
+                      Active
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 1</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 2</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 3</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNav>
+              </SectionContainer>
+            </MDBCol>
+          </MDBRow>
+        </SectionContainer>
 
-        <MDBContainer className="mt-4">
-          <MDBNav pills color="unique">
-            <MDBNavItem>
-              <MDBNavLink active to="#!">Active</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 1</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 2</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 3</MDBNavLink>
-            </MDBNavItem>
-          </MDBNav>
-        </MDBContainer>
+        <SectionContainer title="Justified Navs with Tabs & Pills" noBorder>
+          <MDBRow>
+            <MDBCol md="6">
+              <SectionContainer className="pb-5">
+                <MDBNav tabs color="purple" className="nav-justified">
+                  <MDBNavItem>
+                    <MDBNavLink active to="#!">
+                      Active
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 1</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 2</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 3</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNav>
+              </SectionContainer>
+            </MDBCol>
+            <MDBCol md="6">
+              <SectionContainer className="pb-4">
+                <MDBNav pills className="nav-justified" className="mb-2">
+                  <MDBNavItem>
+                    <MDBNavLink active to="#!">
+                      Active
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 1</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 2</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link 3</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNav>
+              </SectionContainer>
+            </MDBCol>
+          </MDBRow>
+        </SectionContainer>
 
-        <h2 className="mt-5">Justified Navs with Tabs & Pills</h2>
-        <MDBContainer className="mt-4">
-          <MDBNav tabs color="purple" className="nav-justified">
-            <MDBNavItem>
-              <MDBNavLink active to="#!">Active</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 1</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 2</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 3</MDBNavLink>
-            </MDBNavItem>
-          </MDBNav>
-        </MDBContainer>
-
-        <MDBContainer className="mt-4">
-          <MDBNav pills className="nav-justified">
-            <MDBNavItem>
-              <MDBNavLink active to="#!">Active</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 1</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 2</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link 3</MDBNavLink>
-            </MDBNavItem>
-          </MDBNav>
-        </MDBContainer>
-
-        <h2 className="mt-5">Navs with Tabs & Pills with dropdown</h2>
-
-        <MDBContainer className="mt-4">
-          <MDBNav tabs color="secondary">
-            <MDBNavItem>
-              <MDBNavLink active to="#!">Active</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBDropdown>
-                <MDBDropdownToggle nav caret color="secondary">
-                  Dropdown
-                </MDBDropdownToggle>
-                <MDBDropdownMenu color="secondary">
-                  <MDBDropdownItem>Action</MDBDropdownItem>
-                  <MDBDropdownItem>Another Action</MDBDropdownItem>
-                  <MDBDropdownItem>Something else here</MDBDropdownItem>
-                  <MDBDropdownItem divider />
-                  <MDBDropdownItem>Separated link</MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink disabled to="#!">Disabled</MDBNavLink>
-            </MDBNavItem>
-          </MDBNav>
-        </MDBContainer>
-
-        <MDBContainer className="mt-4">
-          <MDBNav pills color="dark">
-            <MDBNavItem>
-              <MDBNavLink active to="#!">Active</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBDropdown>
-                <MDBDropdownToggle nav caret color="dark">
-                  Dropdown
-                </MDBDropdownToggle>
-                <MDBDropdownMenu color="dark">
-                  <MDBDropdownItem>Action</MDBDropdownItem>
-                  <MDBDropdownItem>Another Action</MDBDropdownItem>
-                  <MDBDropdownItem>Something else here</MDBDropdownItem>
-                  <MDBDropdownItem divider />
-                  <MDBDropdownItem>Separated link</MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink to="#!">Link</MDBNavLink>
-            </MDBNavItem>
-            <MDBNavItem>
-              <MDBNavLink disabled to="#!">Disabled</MDBNavLink>
-            </MDBNavItem>
-          </MDBNav>
-        </MDBContainer>
-
+        <SectionContainer title="Navs with Tabs & Pills with dropdown" noBorder>
+          <MDBRow>
+            <MDBCol md="6">
+              <SectionContainer className="pb-5">
+                <MDBNav tabs color="secondary">
+                  <MDBNavItem>
+                    <MDBNavLink active to="#!">
+                      Active
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBDropdown>
+                      <MDBDropdownToggle nav caret color="secondary">
+                        Dropdown
+                      </MDBDropdownToggle>
+                      <MDBDropdownMenu color="secondary">
+                        <MDBDropdownItem>Action</MDBDropdownItem>
+                        <MDBDropdownItem>Another Action</MDBDropdownItem>
+                        <MDBDropdownItem>Something else here</MDBDropdownItem>
+                        <MDBDropdownItem divider />
+                        <MDBDropdownItem>Separated link</MDBDropdownItem>
+                      </MDBDropdownMenu>
+                    </MDBDropdown>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink disabled to="#!">
+                      Disabled
+                    </MDBNavLink>
+                  </MDBNavItem>
+                </MDBNav>
+              </SectionContainer>
+            </MDBCol>
+            <MDBCol md="6">
+              <SectionContainer className="pb-4">
+                <MDBNav pills color="dark" className="mb-2">
+                  <MDBNavItem>
+                    <MDBNavLink active to="#!">
+                      Active
+                    </MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBDropdown>
+                      <MDBDropdownToggle nav caret color="dark">
+                        Dropdown
+                      </MDBDropdownToggle>
+                      <MDBDropdownMenu color="dark">
+                        <MDBDropdownItem>Action</MDBDropdownItem>
+                        <MDBDropdownItem>Another Action</MDBDropdownItem>
+                        <MDBDropdownItem>Something else here</MDBDropdownItem>
+                        <MDBDropdownItem divider />
+                        <MDBDropdownItem>Separated link</MDBDropdownItem>
+                      </MDBDropdownMenu>
+                    </MDBDropdown>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#!">Link</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink disabled to="#!">
+                      Disabled
+                    </MDBNavLink>
+                  </MDBNavItem>
+                </MDBNav>
+              </SectionContainer>
+            </MDBCol>
+          </MDBRow>
+        </SectionContainer>
       </MDBContainer>
     );
   }
 }
 
 export default NavsPage;
-
