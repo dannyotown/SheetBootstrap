3686d9167f20d5dd5c1a25f5b8e8c01ce2afce63
Aliaksandr <a.andrasiuk@mdbootstrap.com>
2019-07-17 12:54:49 +0200

    Change layout for CollapsePage/pro

--- docs/pages/pro/CollapsePage.js
+++ docs/pages/pro/CollapsePage.js
@@ -653,7 +653,7 @@ class AccordionPage extends Component {
         {/* Accordion with a photo in the background */}
         <SectionContainer header="Accordion with a photo in background">
           <MDBCard
-            className="card-image mb-5"
+            className="card-image"
             style={{
               backgroundImage: `url(https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20%2814%29.jpg)`
             }}
