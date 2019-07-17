8e6844c842912953f41e409413bafc60f4cf90df
Aliaksandr <a.andrasiuk@mdbootstrap.com>
2019-07-17 10:47:47 +0200

    Change layout for StepeprPage/pro

--- docs/pages/pro/StepperPage.js
+++ docs/pages/pro/StepperPage.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBStepper, MDBStep, MDBIcon, MDBBtn, MDBCard, MDBCardBody, MDBInput } from "mdbreact";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBStepper,
+  MDBStep,
+  MDBIcon,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBInput
+} from "mdbreact";
 import DocsLink from "../../components/docsLink";
-
+import SectionContainer from "../../components/sectionContainer";
 class StepperPage extends Component {
-
-  state={
+  state = {
     formActivePanel1: 1,
     formActivePanel1Changed: false,
     formActivePanel2: 1,
     formActivePanel2Changed: false,
     formActivePanel3: 1,
     formActivePanel3Changed: false
-  }
+  };
 
   swapFormActive = a => param => e => {
     this.setState({
@@ -40,12 +50,9 @@ class StepperPage extends Component {
   render() {
     return (
       <MDBContainer>
-        <DocsLink
-          title="Steppers"
-          href="https://mdbootstrap.com/docs/react/components/stepper/"
-        />
-        <MDBContainer className="mt-5">
-          <h2>Horizontal stepper</h2>
+        <DocsLink title="Steppers" href="https://mdbootstrap.com/docs/react/components/stepper/" />
+
+        <SectionContainer header="Horizontal">
           <MDBStepper>
             <MDBStep className="completed">
               <a href="#!">
@@ -68,10 +75,11 @@ class StepperPage extends Component {
               </a>
             </MDBStep>
           </MDBStepper>
+        </SectionContainer>
 
-          <hr className="my-5" />
-          <h2>Vertical stepper</h2>
+        <hr className="my-5" />
 
+        <SectionContainer header="Vertical">
           <MDBStepper vertical>
             <MDBStep className="completed">
               <a href="#!">
@@ -86,22 +94,19 @@ class StepperPage extends Component {
               </a>
               <div className="step-content grey lighten-4">
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse
-                  cupiditate voluptate facere iusto quaerat vitae excepturi,
-                  accusantium ut aliquam repellat atque nesciunt nostrum
-                  similique. Inventore nostrum ut, nobis porro sapiente.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse cupiditate voluptate facere iusto
+                  quaerat vitae excepturi, accusantium ut aliquam repellat atque nesciunt nostrum similique.
+                  Inventore nostrum ut, nobis porro sapiente.
                 </p>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Dolore error excepturi veniam nemo repellendus, distinctio
-                  soluta vitae at sit saepe. Optio eaque quia excepturi adipisci
-                  pariatur totam, atque odit fugiat.
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore error excepturi veniam nemo
+                  repellendus, distinctio soluta vitae at sit saepe. Optio eaque quia excepturi adipisci pariatur
+                  totam, atque odit fugiat.
                 </p>
                 <p>
-                  Deserunt voluptatem illum quae nisi soluta eum perferendis
-                  nesciunt asperiores tempore saepe reiciendis, vero quod a
-                  dolor corporis natus qui magni quas fuga rem excepturi
-                  laboriosam. Quisquam expedita ab fugiat.
+                  Deserunt voluptatem illum quae nisi soluta eum perferendis nesciunt asperiores tempore saepe
+                  reiciendis, vero quod a dolor corporis natus qui magni quas fuga rem excepturi laboriosam.
+                  Quisquam expedita ab fugiat.
                 </p>
               </div>
             </MDBStep>
@@ -126,11 +131,12 @@ class StepperPage extends Component {
               </MDBCol>
             </MDBRow>
           </MDBStepper>
+        </SectionContainer>
 
-          <hr className="my-5" />
-          <h2>Steps within form</h2>
+        <hr className="my-5" />
 
-          <MDBRow>
+        <SectionContainer header="Stepper with form">
+          <MDBRow className="justify-content-center d-flex">
             <MDBCol xl="6" lg="7" md="10">
               <MDBCard>
                 <MDBCardBody>
@@ -140,14 +146,7 @@ class StepperPage extends Component {
                   <MDBStepper form>
                     <MDBStep form>
                       <a href="#formstep1" onClick={this.swapFormActive(1)(1)}>
-                        <MDBBtn
-                          color={
-                            this.state.formActivePanel1 === 1
-                              ? "indigo"
-                              : "default"
-                          }
-                          circle
-                        >
+                        <MDBBtn color={this.state.formActivePanel1 === 1 ? "indigo" : "default"} circle>
                           1
                         </MDBBtn>
                       </a>
@@ -155,14 +154,7 @@ class StepperPage extends Component {
                     </MDBStep>
                     <MDBStep form>
                       <a href="#formstep2" onClick={this.swapFormActive(1)(2)}>
-                        <MDBBtn
-                          color={
-                            this.state.formActivePanel1 === 2
-                              ? "indigo"
-                              : "default"
-                          }
-                          circle
-                        >
+                        <MDBBtn color={this.state.formActivePanel1 === 2 ? "indigo" : "default"} circle>
                           2
                         </MDBBtn>
                       </a>
@@ -170,14 +162,7 @@ class StepperPage extends Component {
                     </MDBStep>
                     <MDBStep form>
                       <a href="#formstep3" onClick={this.swapFormActive(1)(3)}>
-                        <MDBBtn
-                          color={
-                            this.state.formActivePanel1 === 3
-                              ? "indigo"
-                              : "default"
-                          }
-                          circle
-                        >
+                        <MDBBtn color={this.state.formActivePanel1 === 3 ? "indigo" : "default"} circle>
                           3
                         </MDBBtn>
                       </a>
@@ -192,11 +177,7 @@ class StepperPage extends Component {
                           <h3 className="font-weight-bold pl-0 my-4">
                             <strong>MDBStep 1</strong>
                           </h3>
-                          <MDBInput
-                            label="First Name"
-                            className="mt-3"
-                            autoFocus={this.calculateAutofocus(1)}
-                          />
+                          <MDBInput label="First Name" className="mt-3" autoFocus={this.calculateAutofocus(1)} />
                           <MDBInput label="Last Name" className="mt-3" />
                           <MDBInput label="Address" type="textarea" rows="2" />
                           <MDBBtn
@@ -214,11 +195,7 @@ class StepperPage extends Component {
                           <h3 className="font-weight-bold pl-0 my-4">
                             <strong>MDBStep 2</strong>
                           </h3>
-                          <MDBInput
-                            label="Company Name"
-                            className="mt-4"
-                            autoFocus={this.calculateAutofocus(1)}
-                          />
+                          <MDBInput label="Company Name" className="mt-4" autoFocus={this.calculateAutofocus(1)} />
                           <MDBInput label="Company Address" className="mt-4" />
                           <MDBBtn
                             color="indigo"
@@ -252,12 +229,7 @@ class StepperPage extends Component {
                           >
                             previous
                           </MDBBtn>
-                          <MDBBtn
-                            color="default"
-                            rounded
-                            className="float-right"
-                            onClick={this.handleSubmission}
-                          >
+                          <MDBBtn color="default" rounded className="float-right" onClick={this.handleSubmission}>
                             submit
                           </MDBBtn>
                         </MDBCol>
@@ -268,36 +240,21 @@ class StepperPage extends Component {
               </MDBCard>
             </MDBCol>
           </MDBRow>
+        </SectionContainer>
 
-          <hr className="my-5" />
-          <h2>Registration form with steps</h2>
+        <hr className="my-5" />
 
+        <SectionContainer header="Registration form with steps" noBorder>
           <MDBCard>
             <MDBCardBody>
               <h2 className="text-center font-weight-bold pt-4 pb-5 mb-2">
                 <strong>Registration form</strong>
               </h2>
               <MDBStepper icon>
-                <MDBStep
-                  icon="folder-open"
-                  stepName="Basic Information"
-                  onClick={this.swapFormActive(2)(1)}
-                />
-                <MDBStep
-                  icon="pencil-alt"
-                  stepName="Personal Data"
-                  onClick={this.swapFormActive(2)(2)}
-                />
-                <MDBStep
-                  icon="image"
-                  stepName="Terms and Conditions"
-                  onClick={this.swapFormActive(2)(3)}
-                />
-                <MDBStep
-                  icon="check"
-                  stepName="Finish"
-                  onClick={this.swapFormActive(2)(4)}
-                />
+                <MDBStep icon="folder-open" stepName="Basic Information" onClick={this.swapFormActive(2)(1)} />
+                <MDBStep icon="pencil-alt" stepName="Personal Data" onClick={this.swapFormActive(2)(2)} />
+                <MDBStep icon="image" stepName="Terms and Conditions" onClick={this.swapFormActive(2)(3)} />
+                <MDBStep icon="check" stepName="Finish" onClick={this.swapFormActive(2)(4)} />
               </MDBStepper>
 
               <form action="" method="post">
@@ -307,11 +264,7 @@ class StepperPage extends Component {
                       <h3 className="font-weight-bold pl-0 my-4">
                         <strong>Basic Information</strong>
                       </h3>
-                      <MDBInput
-                        label="Email"
-                        className="mt-4"
-                        autoFocus={this.calculateAutofocus(2)}
-                      />
+                      <MDBInput label="Email" className="mt-4" autoFocus={this.calculateAutofocus(2)} />
                       <MDBInput label="Username" className="mt-4" />
                       <MDBInput label="Password" className="mt-4" />
                       <MDBInput label="Repeat Password" className="mt-4" />
@@ -330,11 +283,7 @@ class StepperPage extends Component {
                       <h3 className="font-weight-bold pl-0 my-4">
                         <strong>Personal Data</strong>
                       </h3>
-                      <MDBInput
-                        label="First Name"
-                        className="mt-3"
-                        autoFocus={this.calculateAutofocus(2)}
-                      />
+                      <MDBInput label="First Name" className="mt-3" autoFocus={this.calculateAutofocus(2)} />
                       <MDBInput label="Second Name" className="mt-3" />
                       <MDBInput label="Surname" className="mt-3" />
                       <MDBInput label="Address" type="textarea" rows="2" />
@@ -367,11 +316,7 @@ class StepperPage extends Component {
                         id="checkbox"
                         autoFocus={this.calculateAutofocus(2)}
                       />
-                      <MDBInput
-                        label="I want to receive newsletter"
-                        type="checkbox"
-                        id="checkbox2"
-                      />
+                      <MDBInput label="I want to receive newsletter" type="checkbox" id="checkbox2" />
                       <MDBBtn
                         color="mdb-color"
                         rounded
@@ -396,9 +341,7 @@ class StepperPage extends Component {
                       <h3 className="font-weight-bold pl-0 my-4">
                         <strong>Finish</strong>
                       </h3>
-                      <h2 className="text-center font-weight-bold my-4">
-                        Registration completed!
-                      </h2>
+                      <h2 className="text-center font-weight-bold my-4">Registration completed!</h2>
                       <MDBBtn
                         color="mdb-color"
                         rounded
@@ -407,12 +350,7 @@ class StepperPage extends Component {
                       >
                         previous
                       </MDBBtn>
-                      <MDBBtn
-                        color="success"
-                        rounded
-                        className="float-right"
-                        onClick={this.handleSubmission}
-                      >
+                      <MDBBtn color="success" rounded className="float-right" onClick={this.handleSubmission}>
                         submit
                       </MDBBtn>
                     </MDBCol>
@@ -421,10 +359,11 @@ class StepperPage extends Component {
               </form>
             </MDBCardBody>
           </MDBCard>
+        </SectionContainer>
 
-          <hr className="my-5" />
-          <h2>Vertical registration form steps</h2>
+        <hr className="my-5" />
 
+        <SectionContainer header="Vertical registration form steps" noBorder>
           <MDBCard>
             <MDBCardBody>
               <MDBRow className="pt-5 justify-content-center">
@@ -448,12 +387,7 @@ class StepperPage extends Component {
                       onClick={this.swapFormActive(3)(3)}
                       vertical
                     />
-                    <MDBStep
-                      icon="check"
-                      stepName="Finish"
-                      onClick={this.swapFormActive(3)(4)}
-                      vertical
-                    />
+                    <MDBStep icon="check" stepName="Finish" onClick={this.swapFormActive(3)(4)} vertical />
                   </MDBStepper>
                 </MDBCol>
 
@@ -463,11 +397,7 @@ class StepperPage extends Component {
                       <h3 className="font-weight-bold pl-0 my-4">
                         <strong>Basic Information</strong>
                       </h3>
-                      <MDBInput
-                        label="Email"
-                        className="mt-4"
-                        autoFocus={this.calculateAutofocus(3)}
-                      />
+                      <MDBInput label="Email" className="mt-4" autoFocus={this.calculateAutofocus(3)} />
                       <MDBInput label="Username" className="mt-4" />
                       <MDBInput label="Password" className="mt-4" />
                       <MDBInput label="Repeat Password" className="mt-4" />
@@ -486,11 +416,7 @@ class StepperPage extends Component {
                       <h3 className="font-weight-bold pl-0 my-4">
                         <strong>Personal Data</strong>
                       </h3>
-                      <MDBInput
-                        label="First Name"
-                        className="mt-3"
-                        autoFocus={this.calculateAutofocus(3)}
-                      />
+                      <MDBInput label="First Name" className="mt-3" autoFocus={this.calculateAutofocus(3)} />
                       <MDBInput label="Second Name" className="mt-3" />
                       <MDBInput label="Surname" className="mt-3" />
                       <MDBInput label="Address" type="textarea" rows="2" />
@@ -523,11 +449,7 @@ class StepperPage extends Component {
                         id="checkbox3"
                         autoFocus={this.calculateAutofocus(3)}
                       />
-                      <MDBInput
-                        label="I want to receive newsletter"
-                        type="checkbox"
-                        id="checkbox4"
-                      />
+                      <MDBInput label="I want to receive newsletter" type="checkbox" id="checkbox4" />
                       <MDBBtn
                         color="mdb-color"
                         rounded
@@ -551,9 +473,7 @@ class StepperPage extends Component {
                       <h3 className="font-weight-bold pl-0 my-4">
                         <strong>Finish</strong>
                       </h3>
-                      <h2 className="text-center font-weight-bold my-4">
-                        Registration completed!
-                      </h2>
+                      <h2 className="text-center font-weight-bold my-4">Registration completed!</h2>
                       <MDBBtn
                         color="mdb-color"
                         rounded
@@ -562,12 +482,7 @@ class StepperPage extends Component {
                       >
                         previous
                       </MDBBtn>
-                      <MDBBtn
-                        color="success"
-                        rounded
-                        className="float-right"
-                        onClick={this.handleSubmission}
-                      >
+                      <MDBBtn color="success" rounded className="float-right" onClick={this.handleSubmission}>
                         submit
                       </MDBBtn>
                     </MDBCol>
@@ -576,7 +491,7 @@ class StepperPage extends Component {
               </MDBRow>
             </MDBCardBody>
           </MDBCard>
-        </MDBContainer>
+        </SectionContainer>
       </MDBContainer>
     );
   }
