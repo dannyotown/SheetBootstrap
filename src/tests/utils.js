9ed08e373b7fe8485cce4f3c5631128c3af47f35
Aliaksandr <a.andrasiuk@mdbootstrap.com>
2019-08-06 14:41:49 +0200

    Slightly refactor utils.js for better readability

--- src/tests/utils.js
+++ src/tests/utils.js
@@ -1,7 +1,8 @@
 import checkPropTypes from 'check-prop-types';
 
-export const findByTestAttr = (component, testAttr) =>
-  component.find(`[data-test="${testAttr}"]`);
+export const findByTestAttr = (component, testAttr) => {
+  return component.find(`[data-test="${testAttr}"]`);
+};
 
 export const checkProps = (component, expectedProps) => {
   const result = checkPropTypes(
@@ -14,8 +15,10 @@ export const checkProps = (component, expectedProps) => {
   expect(result).toBeUndefined();
 };
 
-export const checkClass = (component, className) =>
-  expect(component.find(`.${className}`).length).toBe(1);
+export const checkClass = (component, className) => {
+  return expect(component.find(`.${className}`).length).toBe(1);
+};
 
-export const checkTag = (component, tag) =>
-  expect(component.is(tag)).toBe(true);
+export const checkTag = (component, tag) => {
+  return expect(component.is(tag)).toBe(true);
+};
