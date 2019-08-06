971cc51703125f9f73d8f8210aceb940d6292018
Aliaksandr <a.andrasiuk@mdbootstrap.com>
2019-08-06 14:43:00 +0200

    Refactor Button tests

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -10,29 +10,6 @@ import {
 import Button from './Button';
 
 const mockCallback = jest.fn();
-const defaultProps = {
-  active: true,
-  action: true,
-  block: true,
-  color: 'primary',
-  disabled: true,
-  download: 'downloadTest',
-  gradient: 'peach',
-  role: 'buttom',
-  type: 'button',
-  outline: true,
-  rounded: true,
-  circle: true,
-  floating: true,
-  flat: true,
-  innerRef: 'refTest',
-  onClick: mockCallback,
-  size: 'sm',
-  social: 'socialTest',
-  tag: 'div',
-  target: 'targetTest',
-  className: 'testClassName'
-};
 
 const setup = (props = {}) => shallow(<Button {...props} />);
 
@@ -54,55 +31,80 @@ describe('<Button />', () => {
   });
 
   test('does not throw warnings with expected props', () => {
-    checkProps(Button, defaultProps);
+    const expectedProps = {
+      active: true,
+      action: true,
+      block: true,
+      color: 'primary',
+      disabled: true,
+      download: 'downloadTest',
+      gradient: 'peach',
+      role: 'buttom',
+      type: 'button',
+      outline: true,
+      rounded: true,
+      circle: true,
+      floating: true,
+      flat: true,
+      innerRef: 'refTest',
+      onClick: mockCallback,
+      size: 'sm',
+      social: 'socialTest',
+      tag: 'div',
+      target: 'targetTest',
+      className: 'testClassName'
+    };
+
+    wrapper = setup(expectedProps);
+    checkProps(wrapper, expectedProps);
   });
 
   test('does not throw warnings with default props', () => {
-    checkProps(Button, {});
+    checkProps(wrapper, {});
   });
 
-  test(`adds custom class passed as props`, () => {
+  test(`adds custom class passed as property`, () => {
     wrapper = setup({ className: 'testClassName' });
 
     checkClass(wrapper, 'testClassName');
   });
 
   test(`invokes callback function passed as a prop after clicking a button`, () => {
-    wrapper = setup({ ...defaultProps });
+    wrapper = setup({ onClick: mockCallback });
     wrapper.simulate('click');
 
     expect(mockCallback).toHaveBeenCalledTimes(1);
   });
 
-  test(`adds custom attributes passed as props`, () => {
+  test(`adds custom attributes passed as property`, () => {
     wrapper = setup({ 'data-custom-attr': 'custom' });
 
-    expect(wrapper.props()['data-custom-attr']).toBe('custom');
+    expect(wrapper.find(`[data-custom-attr="custom"]`).length).toBe(1);
   });
 
   test('sets href', () => {
     wrapper = setup({ href: 'https://mdbootstrap.com/' });
 
-    expect(wrapper.props()['href']).toBe('https://mdbootstrap.com/');
+    expect(wrapper.prop('href')).toBe('https://mdbootstrap.com/');
+    expect(wrapper.find('[href="https://mdbootstrap.com/"]').length).toBe(1);
   });
 
   test('sets custom wrapper tag', () => {
     wrapper = setup({ tag: 'a' });
-
     checkTag(wrapper, 'a');
   });
 
-  describe('Disabled', () => {
+  describe('disabled', () => {
     beforeEach(() => {
       wrapper = setup({ disabled: true });
-    })
-    
+    });
+
     test('adds disabled class', () => {
       checkClass(wrapper, 'disabled');
-    })
-    
+    });
+
     test('doesn`t render a waves ripple element', () => {
-      expect(findByTestAttr(wrapper, 'waves').length).toBe(0);
-    })
-  })
+      expect(wrapper.find('Waves').length).toBe(0);
+    });
+  });
 });
