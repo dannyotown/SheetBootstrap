0915ef03b51f796ecf80729460c0396da08964d0
Aliaksandr <a.andrasiuk@mdbootstrap.com>
2019-08-08 15:30:20 +0200

    Add tests from InputNumeric

---
+++ src/components/InputNumeric/InputNumeric.test.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps, checkClass } from '../../tests/utils';
+import checkPropTypes from 'check-prop-types';
+import InputNumeric from './InputNumeric';
+
+const setup = (props = {}) => shallow(<InputNumeric {...props} src='test' />);
+
+describe('<InputNumeric />', () => {
+  let wrapper;
+  const mockFn = jest.fn(value => value);
+
+  beforeEach(() => {
+    wrapper = setup();
+  });
+
+  test(`renders`, () => {
+    const InputNumeric = findByTestAttr(wrapper, 'input-numeric');
+    expect(InputNumeric.length).toBe(1);
+  });
+
+  test(`renders without errors`, () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<InputNumeric src='test' />, div);
+  });
+
+  test('does not throw warnings with expected props', () => {
+    const expectedProps = {
+      className: '',
+      getValue: mockFn
+    };
+
+    wrapper = setup(expectedProps);
+    checkProps(wrapper, expectedProps);
+  });
+
+  test('does not throw warnings without props', () => {
+    checkPropTypes(wrapper);
+  });
+
+  test('calls getValue after onChange', () => {
+    wrapper = setup({ getValue: mockFn });
+
+    wrapper.simulate('change', 'testValue');
+
+    expect(mockFn).toHaveBeenCalled();
+  });
+
+  test('returns correct value from getValue method', () => {
+    wrapper = setup({ getValue: mockFn });
+
+    wrapper.simulate('change', 'testValue');
+    const value = mockFn.mock.calls[0][0];
+
+    expect(value).toEqual('testValue');
+  });
+
+  describe('sets classes', () => {
+    test('adds form-control class by default', () => {
+      checkClass(wrapper, 'form-control');
+    });
+
+    test(`adds custom class passed as property`, () => {
+      wrapper = setup({ className: 'testClassName' });
+
+      checkClass(wrapper, 'testClassName');
+    });
+  });
+});
