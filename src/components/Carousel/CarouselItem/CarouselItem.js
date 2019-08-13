761af0a4a4ff42254fb34d90017abdca556547bb
Aliaksandr <a.andrasiuk@mdbootstrap.com>
2019-08-13 07:53:14 +0200

    Add tests for CarouselItem

--- src/components/Carousel/CarouselItem/CarouselItem.js
+++ src/components/Carousel/CarouselItem/CarouselItem.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 class CarouselItem extends Component {
   constructor(props) {
@@ -12,21 +12,21 @@ class CarouselItem extends Component {
 
   moveForward() {
     this.style = {
-      position: "absolute",
-      left: "100%"
+      position: 'absolute',
+      left: '100%'
     };
   }
 
   moveBackwards() {
     this.style = {
-      position: "absolute",
-      left: "-100%"
+      position: 'absolute',
+      left: '-100%'
     };
   }
 
   makeVisible() {
     this.style = {
-      left: "0"
+      left: '0'
     };
   }
 
@@ -43,12 +43,11 @@ class CarouselItem extends Component {
     itemId = parseInt(itemId, 10);
 
     let classes = classNames(
-      "carousel-item",
-      this.context.slide
-        ? "active carousel-slide-item"
-        : itemId === this.context.activeItem
-          ? "active"
-          : false,
+      'carousel-item',
+      {
+        'active carousel-slide-item': this.context.slide,
+        'active': !this.context.slide && itemId === this.context.activeItem
+      },
       className
     );
 
@@ -65,7 +64,12 @@ class CarouselItem extends Component {
     }
 
     return (
-      <Tag {...attributes} className={classes} style={this.style}>
+      <Tag
+        data-test='carousel-item'
+        {...attributes}
+        className={classes}
+        style={this.style}
+      >
         {children}
       </Tag>
     );
@@ -81,7 +85,7 @@ CarouselItem.propTypes = {
 };
 
 CarouselItem.defaultProps = {
-  tag: "div"
+  tag: 'div'
 };
 
 CarouselItem.contextTypes = {
