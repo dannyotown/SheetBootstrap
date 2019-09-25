3b75b7f674ad62a389649f03a083b941c7d7f426
kuba <jakub.chmura@wp.pl>
2019-09-25 13:24:21 +0200

    Refactor handleProps function.

--- src/components/pro/ChipsInput/ChipsInput.test.js
+++ src/components/pro/ChipsInput/ChipsInput.test.js
@@ -115,6 +115,17 @@ describe('<ChipsInput />', () => {
 
       expect(wrapper.state('isTouched')).toEqual(false);
     });
+
+    test('invoke handleAdd() on input', () => {
+      const cb = jest.fn();
+      wrapper = mounted().setState({ chipsList: ['test', 'test2'], handleAdd: cb });
+      const input = wrapper.find('input');
+      input.simulate('change', { target: { value: 'setValue' } });
+      input.simulate('keyUp', { which: 13 });
+      expect(cb).toHaveBeenCalledTimes(1);
+    });
+
+
   });
 
   describe('sets classes', () => {
