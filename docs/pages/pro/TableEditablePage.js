d10ab9cbef2d49b0e9a32acc25e2de6d5b96465e
k.stepien <k.stepien@mdbootstrap.com>
2019-11-04 09:36:48 +0100

    Add getValue and onChange property for TableEditable

--- docs/pages/pro/TableEditablePage.js
+++ docs/pages/pro/TableEditablePage.js
@@ -1,34 +1,44 @@
-import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardHeader, MDBCardBody, MDBTableEditable } from "mdbreact";
-import DocsLink from "./../../components/docsLink";
+import React from 'react';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardHeader,
+  MDBCardBody,
+  MDBTableEditable
+} from 'mdbreact';
+import DocsLink from './../../components/docsLink';
 
-const columns = ["Person Name", "Age", "Company Name", "Country", "City"];
+const columns = ['Person Name', 'Age', 'Company Name', 'Country', 'City'];
 
 const data = [
-  ["Aurelia Vega", 30, "Deepends", "Spain", "Madrid"],
-  ["Guerra Cortez", 45, "Insectus", "USA", "San Francisco"],
-  ["Guadalupe House", 26, "Isotronic", "Germany", "Frankfurt am Main"],
-  ["Elisa Gallagher", 31, "Portica", "United Kingdom", "London"]
+  ['Aurelia Vega', 30, 'Deepends', 'Spain', 'Madrid'],
+  ['Guerra Cortez', 45, 'Insectus', 'USA', 'San Francisco'],
+  ['Guadalupe House', 26, 'Isotronic', 'Germany', 'Frankfurt am Main'],
+  ['Elisa Gallagher', 31, 'Portica', 'United Kingdom', 'London']
 ];
 
 const TableEditablePage = props => {
   return (
-    <MDBContainer className="mt-3">
+    <MDBContainer className='mt-3'>
       <DocsLink
-        title="Table Editable"
-        href="https://mdbootstrap.com/docs/react/tables/editable/"
+        title='Table Editable'
+        href='https://mdbootstrap.com/docs/react/tables/editable/'
       />
-      <MDBRow className="py-3">
-        <MDBCol md="12">
+      <MDBRow className='py-3'>
+        <MDBCol md='12'>
           <MDBCard>
-            <MDBCardHeader
-              tag="h3"
-              className="text-center text-uppercase py-4"
-            >
+            <MDBCardHeader tag='h3' className='text-center text-uppercase py-4'>
               Table Editable
             </MDBCardHeader>
             <MDBCardBody>
-              <MDBTableEditable data={data} columns={columns} striped bordered />
+              <MDBTableEditable
+                data={data}
+                columns={columns}
+                striped
+                bordered
+              />
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
