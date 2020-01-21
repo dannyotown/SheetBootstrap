ddf26020c4fa394b47c06206d0f10b3c57684103
p.glejzer <p.glejzer@mdbootstrap.com>
2020-01-21 14:25:23 +0100

    update test

--- src/components/DataTable/DataTable.test.js
+++ src/components/DataTable/DataTable.test.js
@@ -124,10 +124,10 @@ describe('<DataTable />', () => {
       theadTextWhite: true,
       tbodyColor: 'test',
       tbodyTextWhite: true,
-      
-      onSearch: ()=>{},
-      onSort: ()=>{},
-      onPageChange: ()=>{}
+
+      onSearch: () => {},
+      onSort: () => {},
+      onPageChange: () => {}
     };
 
     wrapper = setup(expectedProps);
@@ -336,13 +336,13 @@ describe('<DataTable />', () => {
 
   test('counts correct amount of pages', () => {
     wrapper = setup({ entries: 1, data: { rows: [] } });
-    expect(wrapper.instance().pagesAmount()).toEqual(0);
+    expect(wrapper.instance().pagesAmount()).toEqual(1);
 
     wrapper = setup({ entries: 2, data: { rows: [{}, {}, {}, {}] } });
-    expect(wrapper.instance().pagesAmount()).toEqual(2);
+    expect(wrapper.instance().pagesAmount()).toEqual(1);
 
     wrapper = setup({ entries: 2, data: { rows: [{}, {}, {}, {}, {}] } });
-    expect(wrapper.instance().pagesAmount()).toEqual(3);
+    expect(wrapper.instance().pagesAmount()).toEqual(1);
   });
 
   test('`handleSearchChange()` invokes `filterRows()` and sets state correctly', done => {
@@ -576,7 +576,7 @@ describe('<DataTable />', () => {
     test('`checkFieldValue()`returns `searchvalue` instead of value', () => {
       expect(
         wrapper.instance().checkFieldValue(sortRows.rows[0], 'link')
-      ).toEqual('Cedric_');
+      ).toEqual('Airic_');
     });
 
     test('`checkField()` returns correct result of sort depending on `sort direction`', () => {
@@ -604,11 +604,11 @@ describe('<DataTable />', () => {
   });
 
   describe('sets classes', () => {
-    test('adds \'dataTables_wrapper dt-bootstrap4\' class by default', () => {
+    test("adds 'dataTables_wrapper dt-bootstrap4' class by default", () => {
       checkClass(wrapper, 'dataTables_wrapper.dt-bootstrap4');
     });
 
-    test('adds \'row flex-row-reverse\' class if (barReverse) property is passed', () => {
+    test("adds 'row flex-row-reverse' class if (barReverse) property is passed", () => {
       wrapper = setup({ barReverse: true });
 
       checkClass(wrapper, 'row.flex-row-reverse');
