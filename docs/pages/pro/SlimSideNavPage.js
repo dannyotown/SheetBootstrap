49a65f2c57d6809567c4258c7e49e1f2839be8c3
Piotr Glejzer <glejzerpiotr@gmail.com>
2020-04-24 11:43:10 +0200

    add new version

--- docs/pages/pro/SlimSideNavPage.js
+++ docs/pages/pro/SlimSideNavPage.js
@@ -19,7 +19,9 @@ import SectionContainer from '../../components/sectionContainer';
 class SideNavPage extends Component {
   state = {
     sideNavLeft: false,
-    sideNavRight: false
+    sideNavRight: false,
+    sideNavLeftV5: false,
+    sideNavRightV5: false
   };
 
   sidenavToggle = sidenavId => () => {
@@ -30,7 +32,7 @@ class SideNavPage extends Component {
   };
 
   render() {
-    const { sideNavRight, sideNavLeft } = this.state;
+    const { sideNavRight, sideNavLeft, sideNavRightV5, sideNavLeftV5 } = this.state;
 
     return (
       <Router>
@@ -132,6 +134,115 @@ class SideNavPage extends Component {
                 </MDBSideNavCat>
               </MDBSideNavNav>
             </MDBSideNav>
+          </SectionContainer>
+
+          <DocsLink title='Slim Sidenav' href='https://mdbootstrap.com/docs/react/navigation/sidenav/' />
+          <SectionContainer header='Slim' flexCenter>
+            <MDBBtn onClick={this.sidenavToggle('LeftV5')}>
+              <MDBIcon size='lg' icon='bars' />
+            </MDBBtn>
+            <MDBBtn onClick={this.sidenavToggle('RightV5')}>
+              <MDBIcon size='lg' icon='bars' />
+            </MDBBtn>
+
+            {/* the left SideNav: */}
+            <MDBSideNavV5
+              slim
+              mask='rgba-blue-strong'
+              triggerOpening={sideNavLeftV5}
+              breakWidth={1300}
+              className='sn-bg-1'
+              id='slim-1'
+            >
+              <li>
+                <div className='logo-wrapper-v5 sn-ad-avatar-wrapper-v5'>
+                  <a href='#!'>
+                    <img
+                      alt=''
+                      src='https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg'
+                      className='rounded-circle'
+                    />
+                    <span>Anna Deynah</span>
+                  </a>
+                </div>
+              </li>
+
+              <MDBSideNavNavV5>
+                <MDBSideNavCatV5 name='Submit blog' id='submit-blog' icon='chevron-right'>
+                  <MDBSideNavLinkV5>Submit listing</MDBSideNavLinkV5>
+                  <MDBSideNavLinkV5>Registration form</MDBSideNavLinkV5>
+                </MDBSideNavCatV5>
+                <MDBSideNavCatV5 name='Instruction' id='instruction' icon='hand-pointer' href='#'>
+                  <MDBSideNavLinkV5>For bloggers</MDBSideNavLinkV5>
+                  <MDBSideNavLinkV5>For authors</MDBSideNavLinkV5>
+                </MDBSideNavCatV5>
+                <MDBSideNavCatV5 name='About' id='about' icon='eye'>
+                  <MDBSideNavLinkV5>Instruction</MDBSideNavLinkV5>
+                  <MDBSideNavLinkV5>Monthly meetings</MDBSideNavLinkV5>
+                </MDBSideNavCatV5>
+                <MDBSideNavCatV5 name='Contact me' id='contact-me' icon='envelope'>
+                  <MDBSideNavLinkV5>FAQ</MDBSideNavLinkV5>
+                  <MDBSideNavLinkV5>Write a message</MDBSideNavLinkV5>
+                </MDBSideNavCatV5>
+              </MDBSideNavNavV5>
+            </MDBSideNavV5>
+
+            {/* the right SideNav: */}
+            <MDBSideNavV5
+              slim
+              triggerOpening={sideNavRightV5}
+              className='side-nav-light-v5'
+              right
+              breakWidth={1300}
+              id='slim-2'
+            >
+              <li>
+                <div className='logo-wrapper-v5 sn-ad-avatar-wrapper-v5'>
+                  <a href='#!'>
+                    <img
+                      alt=''
+                      src='https://mdbootstrap.com/img/Photos/Avatars/avatar-1-mini.jpg'
+                      className='rounded-circle'
+                    />
+                    <span className='text-black-50'>John Smith</span>
+                  </a>
+                </div>
+              </li>
+              <li>
+                <ul className='social'>
+                  <li>
+                    <MDBIcon brand icon='facebook' />
+                  </li>
+                  <li>
+                    <MDBIcon brand icon='pinterest' />
+                  </li>
+                  <li>
+                    <MDBIcon brand icon='google-plus' />
+                  </li>
+                  <li>
+                    <MDBIcon brand icon='twitter' />
+                  </li>
+                </ul>
+              </li>
+              <MDBSideNavNavV5>
+                <MDBSideNavCatV5 name='Submit blog' id='submit-blog2' icon='chevron-right'>
+                  <MDBSideNavLinkV5 className='active'>Submit listing</MDBSideNavLinkV5>
+                  <MDBSideNavLinkV5>Registration form</MDBSideNavLinkV5>
+                </MDBSideNavCatV5>
+                <MDBSideNavCatV5 name='Instruction' id='instruction2' icon='hand-pointer'>
+                  <MDBSideNavLinkV5>For bloggers</MDBSideNavLinkV5>
+                  <MDBSideNavLinkV5>For authors</MDBSideNavLinkV5>
+                </MDBSideNavCatV5>
+                <MDBSideNavCatV5 name='About' id='about2' icon='eye'>
+                  <MDBSideNavLinkV5>Instruction</MDBSideNavLinkV5>
+                  <MDBSideNavLinkV5>Monthly meetings</MDBSideNavLinkV5>
+                </MDBSideNavCatV5>
+                <MDBSideNavCatV5 name='Contact me' id='contact-me2' icon='envelope'>
+                  <MDBSideNavLinkV5>FAQ</MDBSideNavLinkV5>
+                  <MDBSideNavLinkV5>Write a message</MDBSideNavLinkV5>
+                </MDBSideNavCatV5>
+              </MDBSideNavNavV5>
+            </MDBSideNavV5>
           </SectionContainer>
         </MDBContainer>
       </Router>
